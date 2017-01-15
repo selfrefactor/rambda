2d5d4253545f94dac15e90abe498680f430092e2
Dejan Toteff <deyan8284@gmail.com>
2017-01-15 19:29:50 +0200

    update

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const Ramda = require("ramda")
 const R = require("./rambda")
 
 describe("common cases", () => {
@@ -6,6 +7,16 @@ describe("common cases", () => {
     expect(
       R.adjust(R.add(1))(1)([1,2,3])
     ).toEqual([1,3,3])
+  })
+
+  it("any",()=>{
+    expect(
+      R.compose(
+        R.any(val=>val<5),
+        R.map(R.subtract(Ramda.__,10)),
+        R.adjust(R.add(1),0)
+      )([0,2,3,4,5,6,7,8,9])
+    ).toEqual(true)
   })
 
   it("any",()=>{
