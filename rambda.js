4e634e67f93d88f56b023f2669b4344310e76436
Dejan Toteff <deyan8284@gmail.com>
2017-01-15 18:45:52 +0200

    update

--- rambda.js
+++ rambda.js
@@ -1,72 +1,182 @@
 const R = require("./compose")
-const round = (num) =>{
-  return Math.round(num * 100) / 100
+
+const add = (a,b)=>{
+	if(b === undefined){
+  	return c => add(a,c)
+  }else{
+  	return a + b
+  }
 }
 
-const range = (start, end) => {
-  let willReturn = []
-  for(let i = start;  i<end; i++){
-    willReturn.push(i);
+const adjust = (fn,index,arr)=>{
+  if(index === undefined){
+    return (indexHolder, arrHolder) => adjust(fn,indexHolder, arrHolder)
+  }else if(arr === undefined){
+    return holder => adjust(fn,index, holder)
+  }else{
+    return arr.map((val,key)=>{
+      if(key===index){
+        return fn(arr[index])
+      }else{
+        return val
+      }
+    })
   }
+}
+
+const any = (fn,arr) =>{
+	if(arr===undefined){
+  	return holder => any(fn,holder)
+  }else{
+  	let flag = false
+  arr.map(val=>{
+  	if(fn(val)===true){
+    	flag = true
+    }
+  })
+  return flag
+  }
+}
+
+const append = (val,arr)=>{
+	if(arr === undefined){
+  	return holder => append(val,holder)
+  }else{
+  	const clone = arr
+  	clone.unshift(val)
+    return clone
+  }
+}
+
+const contains = (val,arr) =>{
+  if(arr===undefined){
+    return holder => contains(val,holder)
+  }else{
+    return any(value=>val===value,arr)
+  }
+}
+
+const filter = (fn,arr) =>{
+	if(arr===undefined){
+  	return holder => filter(fn,holder)
+  }else{
+  	return arr.filter(fn)
+  }
+}
+
+const flatten = arr => {
+  const willReturn = []
+  arr.map(val => {
+    if (val instanceof Array) {
+      val.map(value => {
+        if (value instanceof Array) {
+          value.map(lastValue => {
+            if (lastValue instanceof Array) {
+              willReturn.push(...lastValue)
+            } else {
+              willReturn.push(lastValue)
+            }
+          })
+        } else {
+          willReturn.push(value)
+        }
+      })
+    } else {
+      willReturn.push(val)
+    }
+  })
+
   return willReturn
 }
 
 const drop = (dropNumber, arr) => {
-  let arrClone =  arr
+  const arrClone = arr
+
   return arrClone.slice(dropNumber)
 }
 
 const dropLast = (dropNumber, arr) => {
-  let arrClone =  arr
-  return arrClone.slice(0,-dropNumber)
+  const arrClone = arr
+
+  return arrClone.slice(0, -dropNumber)
 }
 
-const head = (arr)=>{
-  return dropLast(arr.length-1,arr)
+const head = arr => dropLast(arr.length - 1, arr)
+
+const init = arr => dropLast(1, arr)
+
+const join = (glue,arr) =>{
+	if(arr===undefined){
+  	return holder => join(glue,holder)
+  }else{
+  	return arr.join(glue)
+  }
 }
 
-const init = (arr)=>{
-  return dropLast(1,arr)
+const map = (fn,arr) =>{
+	if(arr===undefined){
+  	return holder => map(fn,holder)
+  }else{
+  	return arr.map(fn)
+  }
 }
 
-const tail = (arr)=>{
-  return drop(1,arr)
+const last = arr => arr[ arr.length - 1 ]
+
+const prepend = (val,arr)=>{
+	if(arr === undefined){
+  	return holder => prepend(val,holder)
+  }else{
+  	const clone = arr
+    clone.push(val)
+  	return clone
+  }
 }
 
-const last = (arr)=>{
-  return arr[arr.length-1]
+const range = (start, end) => {
+  const willReturn = []
+  for (let i = start; i < end; i++) {
+    willReturn.push(i)
+  }
+
+  return willReturn
 }
 
-const flatten = (arr)=>{
- const willReturn = []
- arr.map(val=>{
-   if(val instanceof Array){
-     val.map(value =>{
-       if(value instanceof Array){
-         value.map(lastValue=>{
-           if(lastValue instanceof Array){
-             willReturn.push(...lastValue)
-           }else{
-             willReturn.push(lastValue)
-           }
-         })
-       }else{
-         willReturn.push(value)
-       }
-     })
-   }else{
-     willReturn.push(val)
-   }
- })
- return willReturn
+const subtract = (a,b)=>{
+	if(b === undefined){
+  	return c => subtract(a,c)
+  }else{
+  	return a - b
+  }
 }
 
-module.exports.round = round
+const split = (glue,str) =>{
+	if(str===undefined){
+  	return holder => split(glue,holder)
+  }else{
+  	return str.split(glue)
+  }
+}
+
+const tail = arr => drop(1, arr)
+
+module.exports.add = add
+module.exports.adjust = adjust
+module.exports.any = any
+module.exports.append = append
+module.exports.compose = R.compose
+module.exports.contains = contains
 module.exports.drop = drop
 module.exports.dropLast = dropLast
+module.exports.filter = filter
+module.exports.flatten = flatten
 module.exports.head = head
 module.exports.init = init
-module.exports.tail = tail
+module.exports.join = join
 module.exports.last = last
-module.exports.flatten = flatten
-module.exports.compose = R.compose
+module.exports.map = map
+module.exports.prepend = prepend
+module.exports.range = range
+module.exports.split = split
+module.exports.subtract = subtract
+module.exports.tail = tail
--- test.js
+++ test.js
@@ -1,7 +1,34 @@
 const R = require("./rambda")
 
 describe("common cases", () => {
-  it("compose", () => {
+
+  it("add/adjust",()=>{
+    expect(
+      R.adjust(R.add(1))(1)([1,2,3])
+    ).toEqual([1,3,3])
+  })
+
+  it("any",()=>{
+    expect(
+      R.compose(
+        R.any(val=>val<5),
+        R.map(R.subtract(10)),
+        R.adjust(R.add(1),0)
+      )([0,2,3,4,5,6,7,8,9])
+    ).toEqual(true)
+  })
+
+  it("map",()=>{
+    expect(
+      R.compose(
+        val => val.reverse(),
+        R.map(R.subtract(10)),
+        R.adjust(R.add(1),0)
+      )([0,2,3,4,5,6,7,8,9])
+    ).toEqual([1,2,3,4,5,6,7,8,9])
+  })
+
+  it("tail/init", () => {
     expect(
       R.compose(
         R.tail,
@@ -10,6 +37,7 @@ describe("common cases", () => {
       )([ [ [ 1, [ 2 ] ] ], [ 3, 4 ] ])
     ).toEqual([ 2, 3 ])
   })
+
   it("flatten", () => {
     expect(
       R.flatten([ 1, 2, 3, [ 4 ] ])
@@ -21,4 +49,5 @@ describe("common cases", () => {
       R.flatten([ 1, [ 2, [ [ [ 3 ] ] ] ], [ 4 ] ])
     ).toEqual([ 1, 2, [ 3 ], 4 ])
   })
+
 })
