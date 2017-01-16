75de612c74b54d774a2d618ba4959812d6b85a62
Dejan Toteff <deyan8284@gmail.com>
2017-01-16 11:14:26 +0200

    update

---
+++ readmeHelper.js
@@ -0,0 +1,56 @@
+const fs = require("fs")
+const R = require("./")
+
+const methods = [
+"add",
+ "adjust",
+ "any",
+ "append",
+ "contains",
+ "drop",
+ "dropLast",
+ "filter",
+ "flatten",
+ "head",
+ "init",
+ "join",
+ "last",
+ "length",
+ "map",
+ "omit",
+ "path",
+ "prepend",
+ "pick",
+ "prop",
+ "propEq",
+ "range",
+ "repeat",
+ "replace",
+ "sort",
+ "sortBy",
+ "split",
+ "splitEvery",
+ "subtract",
+ "tail",
+ "take",
+ "takeLast",
+ "test",
+ "toLower",
+ "toUpper",
+ "type",
+ "values",
+ "uniq",
+ "update",
+]
+
+const willSave = R.compose(
+  R.join("\n\n"),
+  R.map(val=>{
+    return `#### ${val}
+
+
+    [link to Ramda's docs for ${val} method](http://ramdajs.com/docs/#${val})`
+  })
+)(methods)
+
+fs.writeFileSync("a.txt",willSave)
