950e38c063dfc0d4a93a3c8a798fd59e07406051
self_refactor <deyan8284@gmail.com>
2017-04-16 07:05:13 +0000

    add curry flip tests

--- rambda.js
+++ rambda.js
@@ -623,7 +623,7 @@ function update(index, newValue, arr){
   return arrClone.fill(newValue, index, index + 1)
 }
 
-function flip (fnToCurry){
+function flipp (fnToCurry){
     return (...curryArguments)=>{
       const len = fnToCurry.length
       if(curryArguments[1]===undefined){
@@ -639,12 +639,38 @@ function flip (fnToCurry){
     }
 }
 
+function flip(fnToCurry){
+  return (...curryArguments)=>{
+    const len = fnToCurry.length
+    if(curryArguments[1]===undefined){
+      if(len > 1){
+        return (...futureArguments) => {
+          if(len === 3 && futureArguments.length === 1){
+            return holder => fnToCurry(holder,futureArguments[0],curryArguments[0]) 
+          }
+         return fnToCurry(...futureArguments.reverse(),curryArguments[0]) 
+        }
+      }
+      return fnToCurry(...curryArguments.reverse())
+    }else if(curryArguments[2]===undefined && len === 3){
+      return (futureArgument) => fnToCurry(futureArgument, ...curryArguments.reverse())
+    }
+
+    return fnToCurry(...curryArguments.reverse())
+  }
+}
+
 function curry(fnToCurry){
   return (...curryArguments)=>{
     const len = fnToCurry.length
     if(curryArguments[1]===undefined){
       if(len > 1){
-        return (...futureArguments) => fnToCurry(curryArguments[0],...futureArguments)
+        return (...futureArguments) => {
+          if(len === 3 && futureArguments.length === 1){
+            return b => fnToCurry(curryArguments[0],futureArguments[0],b) 
+          }
+         return fnToCurry(curryArguments[0],...futureArguments) 
+        }
       }
       return fnToCurry(...curryArguments)
     }else if(curryArguments[2]===undefined && len === 3){
@@ -655,6 +681,10 @@ function curry(fnToCurry){
   }
 }
 
+
+
+exports.flip = flip
+exports.curry = curry
 exports.add = add
 exports.adjust = adjust
 exports.any = any
--- test.js
+++ test.js
@@ -1,7 +1,128 @@
 const Ramda = require("ramda")
 const R = require("./rambda")
 
+describe("flip", () => {
+  const update = R.flip(R.update)
+  it("no curring", () => {
+    expect(
+      update([0,7,2],1,1)
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '1' '2'", () => {
+    expect(
+      update([0,7,2],1)(1)
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '2' '1'", () => {
+    expect(
+      update([0,7,2])(1,1)
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '1' '1' '1", () => {
+    expect(
+      update([0,7,2])(1)(1)
+    ).toEqual([ 0,1,2 ])
+  })
+})
+
+
+describe("flip", () => {
+  function updateFn(index, newValue, arr){
+    const arrClone = Array.from(arr)
+    return arrClone.fill(newValue, index, index + 1)
+  }
+  const update = R.flip(updateFn)
+  it("no curring", () => {
+    expect(
+      update([0,7,2],1,1)
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '1' '2'", () => {
+    expect(
+      update([0,7,2],1)(1)
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '2' '1'", () => {
+    expect(
+      update([0,7,2])(1,1)
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '1' '1' '1", () => {
+    expect(
+      update([0,7,2])(1)(1)
+    ).toEqual([ 0,1,2 ])
+  })
+})
+
+describe("curry", () => {
+  function updateFn(index, newValue, arr){
+    const arrClone = Array.from(arr)
+    return arrClone.fill(newValue, index, index + 1)
+  }
+  const update = R.curry(updateFn)
+  it("no curring", () => {
+    expect(
+      update(1,1,[0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '1' '2'", () => {
+    expect(
+      update(1)(1,[0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '2' '1'", () => {
+    expect(
+      update(1,1)([0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curring type '1' '1' '1", () => {
+    expect(
+      update(1)(1)([0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+})
+
 describe("common cases", () => {
+  it("curry", () => {
+    const up = R.curry(R.update)(1,1)
+    expect(
+      up([0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curry", () => {
+    const up = R.curry(R.update)(1)
+    expect(
+      up(1)([0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+  
+  it("curry", () => {
+    expect(
+      R.curry(R.update)(1)(1)([0,7,2])
+    ).toEqual([ 0,1,2 ])
+  })
+})
+
+describe("common cases", () => {
+  it("flip", () => {
+    expect(
+      R.compose(
+        R.map(R.flip(R.subtract)(10)),
+        R.adjust(R.add(1), 0)
+      )([ 0, 2, 3, 4, 5, 6, 7, 8, 9 ])
+    ).toEqual([ -9, -8, -7, -6, -5, -4, -3, -2, -1 ])
+  })
+  
   it("works with Ramda's flip", () => {
     expect(
       R.compose(
