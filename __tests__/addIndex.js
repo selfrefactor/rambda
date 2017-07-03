aecdb2f15a85e93c32845e1edf1994a4068d21e0
JP Gorman <j.p.g.gorman@mgail.com>
2017-07-03 18:55:27 +0100

    Allow addIndex to work on variadic functions

--- __tests__/addIndex.js
+++ __tests__/addIndex.js
@@ -1,6 +1,17 @@
 const R = require("../rambda")
 
-describe.only("addIndex", () => {
+describe("addIndex", () => {
+
+  it("should add index as last argument of function applied to functor", () => {
+    const mockFn = jest.fn()
+    const functor = (fn, a, b, c) => fn(a, b, c)
+    const withIndex = R.addIndex(functor)
+    withIndex(mockFn, "A", "B", "C", "D")
+    expect(
+      mockFn.mock.calls[0]
+    ).toEqual(["A", "B", "C", 0])
+  })
+
   it("should add incrementing index to functors first arity method ", () => {
     expect(
       R.addIndex(R.map)((val, index) => `${val} - ${index}`, ["A", "B", "C"])
--- modules/addIndex.js
+++ modules/addIndex.js
@@ -1,7 +1,7 @@
 function addIndex(functor){
   return function(fn, ...rest) {
     let cnt = 0
-    const newFn = (args) => fn.apply(null, [args, cnt++])
+    const newFn = (...args) => fn.apply(null, [...args, cnt++])
     return functor.apply(null, [newFn, ...rest])
   }
 }
