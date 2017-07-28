f20efc15ae223d11d829c9df505dbba2bc12ef95
WhoAteDaCake <doitsimplefy@gmail.com>
2017-07-28 23:24:11 +0100

    Split up the type.js tests Cleaned up the R.type function

--- __tests__/type.js
+++ __tests__/type.js
@@ -1,51 +1,8 @@
 const R = require("../rambda")
 
 describe("type", () => {
-  it("", () => {
-    const fn1 = () => {}
-    const fn2 = function () {}
-    function fn3 () {}
-    [
-      () => {},
-      fn1,
-      fn2,
-      fn3,
-    ].map(val => {
-      expect(
-        R.type(val)
-      ).toEqual("Function")
-    })
-
-    async function fn4 () {
-      const a = await R.add(1, 2)
-
-      return a
-    }
-
-    [
-      async () => {},
-      fn4,
-    ].map(val => {
-      expect(
-        R.type(val)
-      ).toEqual("Async")
-    })
-
-    const delay = ms => new Promise(resolve=>{
-      setTimeout(function () {
-        resolve(ms+110)
-      }, ms);
-    })
-    
-    expect(
-      R.type(delay(10))
-    ).toEqual("Promise")
-    
-    expect(
-      R.type({})
-    ).toEqual("Object")
-
-    expect(
+  it("returns basic types", () => {
+     expect(
       R.type(1)
     ).toEqual("Number")
 
@@ -62,15 +19,79 @@ describe("type", () => {
     ).toEqual("Null")
 
     expect(
+      R.type(undefined)
+    ).toEqual("Undefined")
+  })
+
+  it("returns Function type", () => {
+    const fn1 = () => {}
+    const fn2 = function () {}
+    function fn3 () {}
+    [
+      () => {},
+      fn1,
+      fn2,
+      fn3,
+    ].map(val => {
+      expect(
+        R.type(val)
+      ).toEqual("Function")
+    })
+  })
+
+  it("returns Async type", () => {
+    async function fn4 () {
+      const a = await R.add(1, 2)
+
+      return a
+    }
+
+    [
+      async () => {},
+      fn4,
+    ].map(val => {
+      expect(
+        R.type(val)
+      ).toEqual("Async")
+    })
+  })
+
+  it("return Promise type", () => {
+    const delay = ms => new Promise(resolve=>{
+      setTimeout(function () {
+        resolve(ms+110)
+      }, ms);
+    })
+    
+    expect(
+      R.type(delay(10))
+    ).toEqual("Promise")
+  })
+
+  it("return Object type", () => {
+    expect(
+      R.type({})
+    ).toEqual("Object")
+  })
+
+  it("return Array type", () => {
+     expect(
       R.type([])
     ).toEqual("Array")
+  })
 
+  it("returns Regexp type", () => {
     expect(
       R.type(/\s/g)
     ).toEqual("RegExp")
-
+  })
+  
+  it("returns custom Class type", () => {
+    class Test {}
+    const testObj = new Test()
+    
     expect(
-      R.type(undefined)
-    ).toEqual("Undefined")
+      R.type(testObj)
+    ).toEqual("Test")
   })
 })
