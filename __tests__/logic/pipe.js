fb893a1b76887b35e1b5b84572518bc643b95c3c
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2017-09-08 14:05:25 +0300

    add R.pipe

---
+++ __tests__/logic/pipe.js
@@ -0,0 +1,12 @@
+const R = require('../../rambda')
+
+describe('pipe', () => {
+  it('', () => {
+    const result = R.pipe(
+      R.map(R.add(1)),
+      R.map(R.add(10)),
+      R.last
+    )([ 1, 2, 3 ])
+    expect(result).toEqual(14)
+  })
+})
---
+++ benchmarks/pipe.js
@@ -0,0 +1,18 @@
+const _ = require('lodash')
+const Benchmark = require('benchmark')
+const R = require('../rambda')
+const Ramda = require('ramda')
+
+const suite = new Benchmark.Suite()
+
+suite.add('Rambda.pipe', () => {
+  R.pipe(val => val.length, val => val + 1)([ 1, 2, 3, 4 ])
+})
+  .add('Ramda', () => {
+    Ramda.pipe(val => val.length, val => val + 1)([ 1, 2, 3, 4 ])
+  })
+  .add('Lodash.flow', () => {
+    _.flow(val => val.length, val => val + 1)([ 1, 2, 3, 4 ])
+  })
+
+module.exports = suite
