a4c5955a4ddcd16c5b860f453ee613d2eb27d921
Jean-Paul Gorman <Jean-Paul.Gorman@parmenion.net>
2017-10-10 13:06:00 +0100

    User internal reduce method

--- modules/without.js
+++ modules/without.js
@@ -1,5 +1,6 @@
 import contains from './contains'
+import reduce from './reduce'
 
 export default function without(itemsToOmit, collection) {
-  return collection.reduce((accum, item) => !contains(item, itemsToOmit) ? accum.concat(item) : accum, [])
+  return reduce((accum, item) => !contains(item, itemsToOmit) ? accum.concat(item) : accum, [], collection)
 }
