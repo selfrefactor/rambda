8e40947f82d991b3d4a17c5887725afac7cddade
Dejan Toteff <deyan8284@gmail.com>
2018-10-14 17:11:26 +0300

    chore@stop: revert to rollup

--- files/minify.js
+++ files/minify.js
@@ -1,3 +1,5 @@
 const { minify } = require('minify')
 
 minify({ filePath : 'dist/rambda.umd.js', output:'_' })
+minify({ filePath : 'dist/rambda.js', output:'_' })
+minify({ filePath : 'dist/rambda.esm.js', output:'_' })
