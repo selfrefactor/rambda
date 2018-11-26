b4c406d6eb659ff1f33047f7f5bce3020a52bbad
Dejan Toteff <deyan8284@gmail.com>
2018-11-27 01:12:50 +0200

    test: sort doesn't mutate

--- src/sort.spec.js
+++ src/sort.spec.js
@@ -9,3 +9,16 @@ test('sort', () => {
 
   expect(sort((a, b) => a - b)([2, 3, 1])).toEqual([1, 2, 3])
 })
+
+test('it doesn\'t mutate', () => {
+  const list = ['foo', 'bar', 'baz']
+  expect(sort((a, b) => a > b, list)).toEqual([
+    'bar',
+    'baz',
+    'foo',
+  ])
+
+  expect(list[0]).toBe('foo')
+  expect(list[1]).toBe('bar')
+  expect(list[2]).toBe('baz')
+})
