b27e21dc1ba97fb500fcd0b9fdd59d81b7340cb1
Dejan Toteff <deyan8284@gmail.com>
2019-01-17 23:22:38 +0200

    test: path works with arrays inside object

--- src/path.spec.js
+++ src/path.spec.js
@@ -1,11 +1,17 @@
 import { path } from './path'
 
+test('with array inside object', () => {
+  const obj = { a : { b : [1,{ c : 1 }]} }
+
+  expect(path('a.b.1.c', obj)).toBe(1)
+})
+
 test('works with undefined', () => {
   const obj = { a : { b : { c : 1 } } }
 
-  expect(path('a.b.c.d.f', obj)).toEqual(undefined)
-  expect(path('foo.babaz', undefined)).toEqual(undefined)
-  expect(path('foo.babaz')(undefined)).toEqual(undefined)
+  expect(path('a.b.c.d.f', obj)).toBeUndefined()
+  expect(path('foo.babaz', undefined)).toBeUndefined()
+  expect(path('foo.babaz')(undefined)).toBeUndefined()
 })
 
 test('works with string instead of array', () => {
@@ -19,9 +25,9 @@ test('path', () => {
     path([ 'foo', 'bar', 'baz' ])({ foo : { bar : { baz : 'yes' } } })
   ).toEqual('yes')
 
-  expect(path([ 'foo', 'bar', 'baz' ])(null)).toEqual(undefined)
+  expect(path([ 'foo', 'bar', 'baz' ])(null)).toBeUndefined()
 
   expect(
     path([ 'foo', 'bar', 'baz' ])({ foo : { bar : 'baz' } })
-  ).toEqual(undefined)
+  ).toBeUndefined()
 })
