7cce4979c3e5a31e674ffa962ff1855ac06b9b29
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2019-07-23 20:54:06 +0200

    feat@identical

---
+++ src/identical.js
@@ -0,0 +1,29 @@
+import _objectIs from './internal/_objectIs'
+
+/**
+ * Returns true if its arguments are identical, false otherwise. Values are
+ * identical if they reference the same memory. `NaN` is identical to `NaN`;
+ * `0` and `-0` are not identical.
+ *
+ * Note this is merely a curried version of ES6 `Object.is`.
+ *
+ * @func
+ * @category Relation
+ * @param {*} a
+ * @param {*} b
+ * @return {Boolean}
+ * @example
+ *
+ *      const o = {};
+ *      R.identical(o, o); //=> true
+ *      R.identical(1, 1); //=> true
+ *      R.identical(1, '1'); //=> false
+ *      R.identical([], []); //=> false
+ *      R.identical(0, -0); //=> false
+ *      R.identical(NaN, NaN); //=> true
+ */
+export function identical(a, b) {
+  if (arguments.length === 1) return b => identical(a, b)
+
+  return _objectIs(a, b)
+}
---
+++ src/identical.spec.js
@@ -0,0 +1,13 @@
+import { identical } from './identical'
+
+test('identical', () => {
+  const a = {}
+
+  expect(identical(100, 100)).toEqual(true)
+  expect(identical(100, '100')).toEqual(false)
+  expect(identical('string', 'string')).toEqual(true)
+  expect(identical([], [])).toEqual(false)
+  expect(identical(a, a)).toEqual(true)
+  expect(identical(undefined, undefined)).toEqual(true)
+  expect(identical(null, undefined)).toEqual(false)
+})
