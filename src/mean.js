1f81caeac37095f79c718666c73eb415ff596cd5
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2019-07-27 11:45:09 +0200

    fix doc

--- src/mean.js
+++ src/mean.js
@@ -5,8 +5,6 @@ import { sum } from './sum'
  * Returns the mean of the given list of numbers.
  *
  * @func
- * @memberOf R
- * @since v0.14.0
  * @category Math
  * @sig [Number] -> Number
  * @param {Array} list
