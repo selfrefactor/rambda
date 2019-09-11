4725401faa93c3de88842c8102c64d95dcf0f05d
Dejan Toteff <deyan8284@gmail.com>
2019-09-12 00:34:15 +0300

    docs@small curry

--- src/curry.js
+++ src/curry.js
@@ -1,5 +1,4 @@
 /**
- * taken from the last comment of https://gist.github.com/mkuklis/5294248
  * Returns a curried equivalent of the provided function. The curried function
  * has two unusual capabilities. First, its arguments needn't be provided one
  * at a time. If `f` is a ternary function and `g` is `R.curry(f)`, the
@@ -9,19 +8,6 @@
  *   - `g(1)(2, 3)`
  *   - `g(1, 2)(3)`
  *   - `g(1, 2, 3)`
- *
- * Secondly, the special placeholder value [`R.__`](#__) may be used to specify
- * "gaps", allowing partial application of any combination of arguments,
- * regardless of their positions. If `g` is as above and `_` is [`R.__`](#__),
- * the following are equivalent:
- *
- *   - `g(1, 2, 3)`
- *   - `g(_, 2, 3)(1)`
- *   - `g(_, _, 3)(1)(2)`
- *   - `g(_, _, 3)(1, 2)`
- *   - `g(_, 2)(1)(3)`
- *   - `g(_, 2)(1, 3)`
- *   - `g(_, 2)(_, 3)(1)`
  *
  * @func
  * @category Function
