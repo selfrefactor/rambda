c83bbe1463b065fd3857032ffe961b8b67202f9b
squidfunk <martin.donath@squidfunk.com>
2019-09-16 20:57:16 +0200

    feat: implemented symmetric difference

---
+++ src/symmetricDifference.js
@@ -0,0 +1,27 @@
+import { concat } from "./concat"
+import { filter } from "./filter"
+import { includes } from "./includes"
+
+/**
+ * Finds the set (i.e. no duplicates) of all elements contained in the first or
+ * second list, but not both.
+ *
+ * @func
+ * @category List
+ * @sig a -> [*] -> [*] -> [*]
+ * @param {Array} list1
+ * @param {Array} list2
+ * @return {Array} The elements in `list1` or `list2`, but not both
+ * @example
+ *
+ *      R.symmetricDifference([1, 2, 3, 4], [3, 4, 5, 6]) // => [1, 2, 5, 6]
+ *      R.symmetricDifference([]); //=> []
+ */
+export function symmetricDifference(list1, list2) {
+  if (arguments.length === 1) return _list => symmetricDifference(list1, _list)
+
+  return concat(
+    filter(value => !includes(value, list2), list1),
+    filter(value => !includes(value, list1), list2)
+  )
+}
