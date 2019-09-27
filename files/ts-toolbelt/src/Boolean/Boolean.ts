d299eb884b5080c858ad54a510cee11bf2208a4a
Dejan Toteff <deyan8284@gmail.com>
2019-09-27 13:47:48 +0300

    chore: move up ts.toolbelt folder

--- files/ts-toolbelt/src/Boolean/Boolean.ts
+++
@@ -1,4 +0,0 @@
-export type Boolean = True | False
-
-export type True  = 1
-export type False = 0
--- files/ts-toolbelt/src/Union/Nullable.ts
+++
@@ -1,9 +0,0 @@
-/** Add **`undefined`** to **`U`**
- * @param U to make nullable
- * @returns **union**
- * @example
- * ```ts
- * ```
- */
-export type Nullable<U extends any> =
-    U | undefined
