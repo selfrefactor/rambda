467e9687ebde72d248aa6744c55a89832911421a
Dejan Toteff <deyan8284@gmail.com>
2019-10-12 16:17:17 +0300

    chore@progress toggle spec content

--- _typings_tests/sortBy-spec.ts
+++ _typings_tests/sortBy-spec.ts
@@ -11,6 +11,7 @@ describe('sortBy', () => {
     const input: Array<Input> = [ { a : 2 }, { a : 1 }, { a : 0 } ]
     const result =  sortBy(fn, input)
 
+    result // $ExpectType Input[]
     result[0].a // $ExpectType number
   });
 });
---
+++ files/createReadme/addToggle.spec.js
@@ -0,0 +1,28 @@
+const input = [ 'zipObj\n',
+  '\n',
+  '> zipObj(a: K[], b: V[]): Object\n',
+  '\n',
+  'It will return a new object with keys of `a` array and values of `b` array.\n',
+  '\n',
+  '```\n',
+  'R.zipObj([\'a\', \'b\', \'c\'], [1, 2, 3])\n',
+  '//=> {a: 1, b: 2, c: 3}\n',
+  '\n',
+  '// truncates to shortest list\n',
+  'R.zipObj([\'a\', \'b\', \'c\'], [1, 2])\n',
+  '//=> {a: 1, b: 2}\n',
+  '```\n',
+  '\n',
+  '[Source](https://github.com/selfrefactor/rambda/tree/master/src/zipObj.js)\n',
+  '\n',
+]
+
+const { addToggle } = require('./addToggle.js')
+
+test('happy', () => {
+  console.log(
+    addToggle(input.join``)
+
+  )
+  // expect().toBe()
+})
