1b13d46075807c64baf951c17720f93f10d42344
Dejan Toteff <deyan8284@gmail.com>
2019-10-16 17:24:53 +0300

    test@small start fix r.drop

--- src/drop.spec.js
+++ src/drop.spec.js
@@ -1,13 +1,17 @@
 import { drop } from './drop'
 
-test('', () => {
-  expect(drop(1, [ 'foo', 'bar', 'baz' ])).toEqual([ 'bar', 'baz' ])
-
+test('with array', () => {
   expect(drop(2)([ 'foo', 'bar', 'baz' ])).toEqual([ 'baz' ])
-
   expect(drop(3, [ 'foo', 'bar', 'baz' ])).toEqual([])
-
   expect(drop(4, [ 'foo', 'bar', 'baz' ])).toEqual([])
+})
 
+test('with string', () => {
   expect(drop(3, 'rambda')).toEqual('bda')
+})
+
+test('1', () => {
+  expect(drop(0, [1, 2, 3])).toEqual([1, 2, 3])
+expect(drop(-1, [1, 2, 3])).toEqual([1, 2, 3])
+// expect(drop(-Infinity, [1, 2, 3])).toEqual([1, 2, 3])
 })
