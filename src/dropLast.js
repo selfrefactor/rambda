28f2c9a25a1fbf96aa71aae00067157286360efd
Dejan Toteff <deyan8284@gmail.com>
2019-10-17 09:59:58 +0300

    feat@method apply r.drop to r.drop.last

--- src/dropLast.js
+++ src/dropLast.js
@@ -19,5 +19,5 @@
 export function dropLast(n, list){
   if (arguments.length === 1) return _list => dropLast(n, _list)
 
-  return list.slice(0, -n)
+  return n > 0 ? list.slice(0, -n) : list.slice()
 }
--- src/dropLast.spec.js
+++ src/dropLast.spec.js
@@ -1,16 +1,24 @@
 import { dropLast } from './dropLast'
+import assert from 'assert'
 
-test('', () => {
-  expect(dropLast(1, [ 'foo', 'bar', 'baz' ])).toEqual([
-    'foo',
-    'bar',
-  ])
-
+test('with array', () => {
   expect(dropLast(2)([ 'foo', 'bar', 'baz' ])).toEqual([ 'foo' ])
-
   expect(dropLast(3, [ 'foo', 'bar', 'baz' ])).toEqual([])
-
   expect(dropLast(4, [ 'foo', 'bar', 'baz' ])).toEqual([])
-
+})
+test('with string', () => {
   expect(dropLast(3, 'rambda')).toEqual('ram')
+})
+
+test('with non-positive count', () => {
+  expect(dropLast(0, [1, 2, 3])).toEqual([1, 2, 3])
+expect(dropLast(-1, [1, 2, 3])).toEqual([1, 2, 3]) 
+expect(dropLast(-Infinity, [1, 2, 3])).toEqual([1, 2, 3])
+})
+
+test('should return copy', () => {
+    var xs = [1, 2, 3];
+
+    assert.notStrictEqual(dropLast(0, xs), xs);
+    assert.notStrictEqual(dropLast(-1, xs), xs);
 })
