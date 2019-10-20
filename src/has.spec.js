7c2314a45b5be34def9bf7e8ac13225d40e68d22
Dejan Toteff <deyan8284@gmail.com>
2019-10-20 13:21:44 +0300

    feat@method r.has with non object

--- src/has.spec.js
+++ src/has.spec.js
@@ -1,6 +1,14 @@
 import { has } from './has'
 
-test('has', () => {
+test('happy', () => {
   expect(has('a')({ a : 1 })).toBeTruthy()
   expect(has('b', { a : 1 })).toBeFalsy()
+})
+
+test('with non-object', () => {
+  expect(has('a', undefined)).toEqual(false)
+  expect(has('a', null)).toEqual(false)
+  expect(has('a', true)).toEqual(false)
+expect(has('a', '')).toEqual(false)
+expect(has('a', /a/)).toEqual(false)
 })
