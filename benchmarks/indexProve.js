07236a4c7aa5d357fec21663f9d96549397daac7
Dejan Toteff <deyan8284@gmail.com>
2019-10-30 20:03:37 +0200

    chore@benchmark r.all

--- benchmarks/indexProve.js
+++ benchmarks/indexProve.js
@@ -1,22 +1,30 @@
 process.env.BENCHMARK_FOLDER = 'benchmarks/benchmark_results'
 const fs = require('fs')
 const path = require('path')
-const {exec, createBenchmark} = require('helpers')
-const {filter} = require('../dist/rambda.js')
-const {mapAsync} = require('rambdax')
+const { exec, createBenchmark } = require('helpers')
+const { filter } = require('../dist/rambda.js')
+const { mapAsync } = require('rambdax')
 
 async function runBenchmarks(){
-  // const allBenchmarks =  filter(x => x !== 'index.js', fs.readdirSync(__dirname))
+  const allBenchmarksxx = filter(x => x !== 'index.js', fs.readdirSync(__dirname))
 
-  const allBenchmarks =  ['add', 'adjust', 'any', 'append', 'assoc', 'compose']
-
+  const allBenchmarks = [
+    'add',
+    'all',
+    'adjust',
+    'any',
+    'append',
+    'assoc',
+    'compose',
+  ]
+  const allBenchmarksx = [ 'all' ] 
   await mapAsync(
     async singleBenchmark => {
-      const required = require(path.join(__dirname, 'add.js'))
+      const required = require(path.join(__dirname, `${singleBenchmark}.js`))
       console.log(singleBenchmark)
-      createBenchmark({[singleBenchmark]: required})
+      createBenchmark({ [ singleBenchmark ] : required })
     }
   )(allBenchmarks)
 }
- 
+
 runBenchmarks()
