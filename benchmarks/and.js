e32d9e5e52afd6c101d508688b399f65d1e523d5
Dejan Toteff <deyan8284@gmail.com>
2019-12-18 23:56:14 +0200

    chore@benchmark r.if.else

--- benchmarks/and.js
+++ benchmarks/and.js
@@ -3,24 +3,24 @@ const R = require('../dist/rambda.js')
 const Ramda = require('ramda')
 
 const and = [
-    {
-        label: 'Rambda',
-        fn: () => {
-            R.and(true, true)
-        }
+  {
+    label : 'Rambda',
+    fn    : () => {
+      R.and(true, true)
     },
-    {
-        label: 'Ramda',
-        fn: () => {
-            Ramda.and(true, true)
-        }
+  },
+  {
+    label : 'Ramda',
+    fn    : () => {
+      Ramda.and(true, true)
     },
-    {
-        label: 'Lodash',
-        fn: () => {
-            _.and(true, true)
-        }
-    }
-];
+  },
+  {
+    label : 'Lodash',
+    fn    : () => {
+      _.and(true, true)
+    },
+  },
+]
 
 module.exports = and
