03e4ac72119a7929b1795fce49078fce449d6da2
Dejan Toteff <deyan8284@gmail.com>
2020-03-18 23:00:47 +0200

    chore@script extract example

---
+++ scripts/populate-docs-data/extract-from-typings/extract-example.js
@@ -0,0 +1,27 @@
+import { mapToObject, match, remove , anyFalse , replace } from 'rambdax'
+
+import { extractName } from './extract-name'
+import { extractRawInfo } from './extract-raw-info'
+
+const skipExampleList = [
+  'mapAsync',
+]
+
+export function extractExample(){
+  const rawInfo = extractRawInfo()
+  
+  return mapToObject(x => {
+    const name = extractName(x)
+    const [ matched ] = match(/Example:(\n|.)+Categories:/m)(x)
+    
+    if(anyFalse(matched, name, !skipExampleList.includes(name))) return
+
+    const exampleRaw = remove([ 'Categories:', 'Example:' ])(matched)
+    const example = replace(
+      '```',
+      '```javascript',
+      exampleRaw
+    )
+    return { [ name ] : example.trim() }
+  })(rawInfo)
+}
