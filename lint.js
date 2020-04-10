b58964073b22376c0061de5ca8fcd5d0c7da3ffb
Dejan Toteff <deyan8284@gmail.com>
2020-04-10 20:42:22 +0300

    feat@start populate methods data

--- lint.js
+++
@@ -1,8 +0,0 @@
-const {lintFn} = require('lint-fn')
-const filePath = `${__dirname}/index.d.ts`
-
-void (async function lint() {
-  console.time('lint')
-  await lintFn(filePath, 'outer')
-  console.timeEnd('lint')
-})()
