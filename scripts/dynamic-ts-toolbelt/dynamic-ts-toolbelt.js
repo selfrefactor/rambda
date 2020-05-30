698c0c2cd5251f5266e84915da6acc47b9ab1f72
Dejan Toteff <deyan8284@gmail.com>
2020-05-30 17:01:37 +0300

    feat: clone repo

--- scripts/dynamic-ts-toolbelt/dynamic-ts-toolbelt.js
+++ scripts/dynamic-ts-toolbelt/dynamic-ts-toolbelt.js
@@ -1,5 +1,19 @@
-import {move} from 'fs-extra'
+import { move } from 'fs-extra'
+import { spawn } from 'helpers-fn'
+import { resolve } from 'path'
 
 export async function dynamicTsToolbelt(){
-  
+  await spawn({
+    cwd     : __dirname,
+    command : 'git',
+    inputs  : [
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/pirix-gh/ts-toolbelt',
+    ],
+  }) 
+  // const sourceDir = resolve(__dirname, '../../node_modules/ts-toolbelt')
+  // const destinationDir = resolve(__dirname, '../../__toolbelt')
+  // await move(sourceDir, destinationDir)
 }
