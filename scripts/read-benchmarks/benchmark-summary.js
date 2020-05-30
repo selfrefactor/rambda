8ba1214eaf6ac17c44d9105b82d77666b18f4967
Dejan Toteff <deyan8284@gmail.com>
2020-05-30 20:53:24 +0300

    service: add summary counter for benchmark results

--- scripts/read-benchmarks/benchmark-summary.js
+++ scripts/read-benchmarks/benchmark-summary.js
@@ -1,7 +1,8 @@
 const { camelCase } = require('string-fn')
 const { readdirSync } = require('fs')
-const { readJsonSync, outputFile } = require('fs-extra')
-const { remove } = require('rambdax')
+
+const { readJsonSync, outputFile, outputJson } = require('fs-extra')
+const { remove, head,  piped,toLower, split } = require('rambdax')
 const { resolve } = require('path')
 const resultsDir = resolve(__dirname,
   '../run-benchmarks/benchmarks/benchmark_results')
@@ -15,6 +16,11 @@ function parseMethodName(input){
 }
 
 export async function benchmarkSummary(){
+  const summaryCounter = {
+    ramda  : 0,
+    rambda : 0,
+    lodash : 0,
+  }
   const allResults = readdirSync(resultsDir)
 
   const tableRows = allResults.map(file => {
@@ -26,13 +32,22 @@ export async function benchmarkSummary(){
 
     results.forEach(result => {
       if (result.name === 'Rambda') rambda = result
-      if (result.name === 'Ramda') ramda = result
-      if (result.name.includes('Lodash')) lodash = result
+      if (result.name === 'Ramda')    ramda = result
+      if (result.name.includes('Lodash'))    summaryCounter.lodash++
     })
 
     const columns = [ rambda, ramda, lodash ]
       .map(x => {
         if (!x) return '🔳'
+        if (x.percentSlower === 0){
+          const winner = piped(
+            x.name,
+            split('.'),
+            head,
+            toLower
+          )
+          summaryCounter[winner]++
+        }
 
         return x.percentSlower === 0 ?
           '🚀 Fastest' :
@@ -46,4 +61,7 @@ export async function benchmarkSummary(){
   const toSave = tableRows.join('\n')
 
   await outputFile(`${ __dirname }/summary.txt`, toSave)
+  await outputJson(
+    `${ __dirname }/summary-counter.json`, summaryCounter, { spaces : 2 }
+  )
 }
