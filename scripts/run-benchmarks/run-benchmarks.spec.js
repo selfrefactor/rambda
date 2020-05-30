1c57087743bd5efeda2dc2e60041d681ea6594ba
Dejan Toteff <deyan8284@gmail.com>
2020-05-30 19:17:40 +0300

    chore: update benchmarks after change in r.curry

--- scripts/run-benchmarks/run-benchmarks.spec.js
+++ scripts/run-benchmarks/run-benchmarks.spec.js
@@ -4,6 +4,6 @@ import { runAllBenchmarks, runSingleBenchmark } from './run-benchmarks.js'
 jest.setTimeout(ms('15 minutes'))
 
 test('happy', async () => {
-  await runAllBenchmarks()
-  // await runSingleBenchmark('add')
+  // await runAllBenchmarks()
+  await runSingleBenchmark('update')
 })
