7842a25d5dad7cf433594cae0a9db3d9d25e42f8
farwayer <farwayer@gmail.com>
2020-06-12 08:27:02 +0300

    can reduce objects, perf improvement

--- source/reduce.js
+++ source/reduce.js
@@ -1,11 +1,35 @@
 import { curry } from './curry'
+import { _isArray } from './_internals/_isArray'
+import { _keys } from './_internals/_keys'
 
 function reduceFn(
   reducer, acc, list
 ){
-  const clone = list.slice()
+  if (list === undefined){
+    return acc
+  }
 
-  return clone.reduce(reducer, acc)
+  if (_isArray(list)){
+    let index = 0
+    const len = list.length
+
+    while (index < len){
+      acc = reducer(acc, list[ index ], index, list)
+      index++
+    }
+  } else {
+    let index = 0
+    const keys = _keys(list)
+    const len = keys.length
+
+    while (index < len){
+      const key = keys[ index ]
+      acc = reducer(acc, key, list[ key ], list)
+      index++
+    }
+  }
+
+  return acc
 }
 
 export const reduce = curry(reduceFn)
