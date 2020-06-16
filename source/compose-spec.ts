629463a86513ced93c802da8242f41da5152c2a2
Dejan Toteff <deyan8284@gmail.com>
2020-06-16 15:01:36 +0300

    test@typings r.compose

---
+++ source/compose-spec.ts
@@ -0,0 +1,20 @@
+import {compose, add, subtract} from 'rambda'
+
+describe('compose', () => {
+  it('happy', () => {
+    const result = compose(
+      subtract(11),
+      add(1),
+      add(1),
+    )(1)
+    result // $ExpectType number
+  })
+
+  it('with void', () => {
+    const result = compose(
+      () => {},
+      () => {}
+    )();
+    result // $ExpectType void
+  })
+})
