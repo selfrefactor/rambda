f5d4c9c29fe910ec35e6b65bed3e2a4363ef6c8c
Dejan Toteff <deyan8284@gmail.com>
2020-07-05 17:59:54 +0300

    test@important remove failing dtslint test for curry first isue

--- source/_typings.spec.js
+++ source/_typings.spec.js
@@ -18,3 +18,4 @@ test('typings are correct', async () => {
 
   expect(status).toBe(0)
 })
+   
--- source/throttle-spec.ts
+++ source/throttle-spec.ts
@@ -1,26 +1,5 @@
 import {
-  add,
   delay,
-  divide,
-  either,
-  endsWith,
-  equals,
-  has,
-  match,
-  hasPath,
-  identical,
-  indexOf,
-  join,
-  ifElse, 
-  mathMod, 
-  is, 
-  last,
-  prepend,
-  lastIndexOf,
-  pipe,
-  range,
-  max,
-  repeat,
   throttle,
 } from 'rambda'
 
@@ -34,15 +13,9 @@ describe('throttle', () => {
     result2 // $ExpectType number
   })
   it('arity of 2', () => {
-    /*
-      If typings of `R.add` are:
-
-      export function add(a: number, b: number): number;
-      export function add(a: number): (b: number) => number;
-
-      then the code below will result in error.
-      The whole issue is descipted in the skipped test suite.
-    */
+    function add(x:number, y: number){
+      return x + y
+    }
     const fn = throttle(add, 1000)
     const result = fn(1, 2)
     result // $ExpectType number
@@ -58,99 +31,3 @@ describe('throttle', () => {
     result // $ExpectType string
   })
 })
-
-/*
-  Stop fixing this problem at R.repeat, so there should be more cases
-  Revert changes as origin of this issue is unknown.
-*/
-describe('curry first issue', () => {
-  it('repeat', () => {
-    const fn = throttle(repeat, 1000)
-    fn(1, 4)
-  })   
-  it('range', () => {
-    const fn = throttle(range, 1000)
-    fn(1, 4)
-  })
-  it('prepend', () => {
-    const fn = throttle(prepend, 1000)
-    fn(1, ['a', 'b', 'c'])
-  })
-  it('mathMod', () => {
-    const fn = throttle(mathMod, 1000)
-    fn(1,2)
-  })
-  it('max', () => {
-    const fn = throttle(max, 1000)
-    fn(1,2)
-  })
-  it('is', () => {
-    const pipeResult = pipe(
-      last,
-      is(Number)
-    )([1, 2, 3])
-    
-    const fn = throttle(is, 1000)
-    const result = fn(Number, 1)
-    result // $ExpectType boolean
-    pipeResult // $ExpectType boolean
-  })
-  it('lastIndexOf', () => {
-    const fn = throttle(lastIndexOf, 1000)
-    fn([1, 2, 3])
-  })
-  it('match', () => {
-    const fn = throttle(match, 1000)
-    fn(/foo/,'foo bar')
-  })
-  it('join', () => {
-    const fn = throttle(join, 1000)
-    const result = fn('|',[1, 2, 3])
-    result // $ExpectType string
-  })
-  it('indexOf', () => {
-    const fn = throttle(indexOf, 1000)
-    const result = fn(1,[1, 2, 3])
-    result // $ExpectType number
-  })
-  it('ifElse', () => {
-    const fn = throttle(ifElse, 1000)
-    const result = fn(x => x > 1, () => 10, () => 2)(3)
-    result // $ExpectType any
-  })
-  it('identical', () => {
-    const fn = throttle(identical, 1000)
-    const result = fn('a', {a:1})
-    result // $ExpectType boolean
-  })
-  it('hasPath', () => {
-    const fn = throttle(hasPath, 1000)
-    const result = fn('a', {a:1})
-    result // $ExpectType boolean
-  })
-  it('has', () => {
-    const fn = throttle(has, 1000)
-    const result = fn('fi', {a:1})
-    result // $ExpectType boolean
-  })
-  it('equals', () => {
-    const fn = throttle(equals, 1000)
-    const result = fn('f','foo')
-    result // $ExpectType boolean
-  })
-  it('endsWith', () => {
-    const fn = throttle(endsWith, 1000)
-    const result = fn('f','foo')
-    result // $ExpectType boolean
-  })
-  it('either', () => {
-    const fn = throttle(either, 1000)
-    const result = fn(x => x> 1,x => x > 4)(5)
-    result // $ExpectType boolean
-  })
-  it('divide', () => {
-    const fn = throttle(divide, 1000)
-    fn(1,2)
-  })
-})
-
