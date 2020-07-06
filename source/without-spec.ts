49daced63ef742a8c2fe04b83296062f83a88e72
Dejan Toteff <deyan8284@gmail.com>
2020-07-06 17:09:49 +0300

    test@typings r.without

---
+++ source/without-spec.ts
@@ -0,0 +1,17 @@
+import {without} from 'rambda'
+
+const itemsToOmit = [ 'A', 'B', 'C' ]
+const collection = [ 'A', 'B', 'C', 'D', 'E', 'F' ]
+
+describe('R.without', () => {
+  it('happy', () => {
+    const result = without(itemsToOmit, collection)
+
+    result // $ExpectType string[]
+  })
+  it('curried', () => {
+    const result = without(itemsToOmit)(collection)
+  
+    result // $ExpectType string[]
+  })
+})
