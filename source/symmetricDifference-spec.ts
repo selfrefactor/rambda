351647207d5caa9f19df86c8bc69069e978c672e
Dejan Toteff <deyan8284@gmail.com>
2020-07-06 21:11:43 +0300

    chore@dnd

---
+++ source/symmetricDifference-spec.ts
@@ -0,0 +1,19 @@
+import {symmetricDifference} from 'rambda'
+
+describe('R.symmetricDifference', () => {
+  it('happy', () => {
+    const list1 = [ 1, 2, 3, 4 ]
+    const list2 = [ 3, 4, 5, 6 ]
+    const result = symmetricDifference(list1, list2)
+
+    result // $ExpectType number[]
+  })
+  
+  it('curried', () => {
+    const list1 = [ { id : 1 }, { id : 2 }, { id : 3 }, { id : 4 } ]
+    const list2 = [ { id : 3 }, { id : 4 }, { id : 5 }, { id : 6 } ]
+    const result = symmetricDifference(list1)(list2)
+    
+    result // $ExpectType { id: number; }[]
+  })
+})
