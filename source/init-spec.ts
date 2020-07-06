97a48f9cffcea115d479e97965d7a962e2ae97b0
Dejan Toteff <deyan8284@gmail.com>
2020-07-06 21:06:24 +0300

    chore@dnd

---
+++ source/init-spec.ts
@@ -0,0 +1,14 @@
+import {init} from 'rambda'
+
+describe('R.init', () => {
+  it('with string', () => {
+    const result = init('foo')
+
+    result // $ExpectType string
+  })
+  it('with list', () => {
+    const result = init([1, 2, 3])
+
+    result // $ExpectType number[]
+  })
+})
