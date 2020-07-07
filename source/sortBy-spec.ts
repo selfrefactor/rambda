57d59b55cbfa0fe2c3b8e9e5f3d7d8cd4fee7bc1
Dejan Toteff <deyan8284@gmail.com>
2020-07-07 11:55:47 +0300

    chore@dnd

--- source/sortBy-spec.ts
+++ source/sortBy-spec.ts
@@ -1,19 +1,37 @@
 import {sortBy} from 'rambda'
 
-describe('sortBy', () => {
-  it('happy', () => {
-    interface Input {
-      a: number,
+interface Input {
+  a: number,
+}
+
+describe('R.sortBy', () => {
+  it('passing type to sort function', () => {
+    function fn(x: any): number {
+      return x.a
+    }
+    function fn2(x: Input): number {
+      return x.a
     }
 
+    const input = [{a: 2}, {a: 1}, {a: 0}]
+    const result = sortBy(fn, input)
+    const curriedResult = sortBy(fn2)(input)
+
+    result // $ExpectType {a: number; } []
+    curriedResult // $ExpectType {a: number; } []
+    result[0].a // $ExpectType number
+  })
+  it('passing type to sort function and list', () => {
     function fn(x: Input): number {
       return x.a
     }
 
     const input: Input[] = [{a: 2}, {a: 1}, {a: 0}]
     const result = sortBy(fn, input)
+    const curriedResult = sortBy(fn)(input)
 
     result // $ExpectType Input[]
+    curriedResult // $ExpectType Input[]
     result[0].a // $ExpectType number
   })
 })
