5d77863feb0a4b148b49b55bca433790ab4d6a32
Dejan Toteff <deyan8284@gmail.com>
2020-07-12 09:28:59 +0300

    chore@dnd

---
+++ source/glue-spec.ts
@@ -0,0 +1,23 @@
+import {glue} from 'rambda'
+
+const baz = 'led zeppelin'
+
+describe('R.glue', () => {
+  it('happy', () => {
+    const result = glue(`
+      Foo
+      ${baz}
+      Bar
+    `)
+    result // $ExpectType string
+  })
+
+  it('with specified glue char', () => {
+    const result = glue(`
+      Foo
+      ${baz}
+      Bar
+    `, '|')
+    result // $ExpectType string
+  })
+})
