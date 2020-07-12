3b5325c087c64ad947d530ea346a299c47d5e9b2
Dejan Toteff <deyan8284@gmail.com>
2020-07-12 15:15:52 +0300

    chore@dnd

---
+++ source/diffference-spec.ts
@@ -0,0 +1,17 @@
+import {difference} from 'rambda'
+
+const list1 = [1,2,3]
+const list2 = [1,2,4]
+
+describe('R.difference', () => {
+  it('happy', () => {
+    const result = difference(list1, list2)
+
+    result // $ExpectType number[]
+  })
+  it('curried', () => {
+    const result = difference(list1)(list2)
+  
+    result // $ExpectType number[]
+  })
+})
--- source/dissoc-spec.ts
+++ source/dissoc-spec.ts
@@ -1,4 +1,4 @@
-import {dissoc} from 'rambda'
+import {dissoc, pipe, identity} from 'rambda'
 
 const obj = {
   a : 1,
@@ -16,6 +16,14 @@ describe('R.dissoc', () => {
   })
   it('curried', () => {
     const result = dissoc<Output>('b')(obj)
+
+    result // $ExpectType Output
+  })
+  it('within R.pipe', () => {
+    const result = pipe<object, object, Output>(
+      identity,
+      dissoc('b')
+    )(obj)
 
     result // $ExpectType Output
   })
