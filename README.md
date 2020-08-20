e6bb434bee51e3b30616158ec2bbcd7e6634ce7f
Dejan Toteff <deyan8284@gmail.com>
2020-08-20 09:29:04 +0300

    docs: add my libraries section

--- README.md
+++ README.md
@@ -22146,4 +22146,33 @@ Approve [PR #266](https://github.com/selfrefactor/rambda/pull/266) that adds `R.
 
 - (https://github.com/stoeffel/awesome-fp-js)[awesome-fp-js]
 
-- (https://github.com/docsifyjs/awesome-docsify)[awesome-docsify]
+- (https://github.com/docsifyjs/awesome-docsify)[awesome-docsify]  
+
+## My other libraries
+
+<table>
+    <tbody>
+        <tr valign="top">
+            <td width="20%" align="center">
+                <h3>Niketa theme</h3>
+                <a href="https://marketplace.visualstudio.com/items?itemName=selfrefactor.Niketa-theme">Collection of 9 light VSCode themes</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>Niketa dark theme</h3>
+                <a href="https://marketplace.visualstudio.com/items?itemName=selfrefactor.niketa-dark-theme">Collection of 9 dark VSCode themes</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>String-fn</h3>
+                <a href="https://github.com/selfrefactor/services/tree/master/packages/string-fn">String utility library</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>Run-fn</h3>
+                <a href="https://github.com/selfrefactor/useful-javascript-libraries">Large collection of JavaScript/Typescript/Angular repos links</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>Run-fn</h3>
+                <a href="https://github.com/selfrefactor/services/tree/master/packages/run-fn">CLI commands for lint JS/TS files and commit git changes</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
--- scripts/populate-readme-data/get-tail.js
+++ scripts/populate-readme-data/get-tail.js
@@ -50,7 +50,41 @@ const templateTail = `
 {{changelog}}
 
 {{additionalInfo}}
+
+{{myLibraries}}
 `
+
+const myLibraries = `
+
+## My other libraries
+
+<table>
+    <tbody>
+        <tr valign="top">
+            <td width="20%" align="center">
+                <h3>Niketa theme</h3>
+                <a href="https://marketplace.visualstudio.com/items?itemName=selfrefactor.Niketa-theme">Collection of 9 light VSCode themes</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>Niketa dark theme</h3>
+                <a href="https://marketplace.visualstudio.com/items?itemName=selfrefactor.niketa-dark-theme">Collection of 9 dark VSCode themes</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>String-fn</h3>
+                <a href="https://github.com/selfrefactor/services/tree/master/packages/string-fn">String utility library</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>Run-fn</h3>
+                <a href="https://github.com/selfrefactor/useful-javascript-libraries">Large collection of JavaScript/Typescript/Angular repos links</a>
+            </td>
+            <td width="20%" align="center">
+                <h3>Run-fn</h3>
+                <a href="https://github.com/selfrefactor/services/tree/master/packages/run-fn">CLI commands for lint JS/TS files and commit git changes</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+`.trim()
 
 export async function getTail(withRambdax){
   const changelogSource = withRambdax ?
@@ -61,6 +95,7 @@ export async function getTail(withRambdax){
 
   return interpolate(templateTail, {
     additionalInfo : getAdditionalInfo(),
+    myLibraries,
     changelog      : changelogContent.toString(),
   })
 }
