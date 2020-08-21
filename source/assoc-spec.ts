dfde5675eeef5f0818dfafca2915179bedd370d8
Dejan Toteff <deyan8284@gmail.com>
2020-08-21 12:53:38 +0300

    test@typings r.assoc

--- source/assoc-spec.ts
+++ source/assoc-spec.ts
@@ -1,9 +1,26 @@
 import {assoc} from 'rambda'
 
+const obj = {a: 1}
+const newValue = 2
+const newProp = 'b'
+
 describe('R.assoc', () => {
   it('happy', () => {
-    const result = assoc('b', 2, {a: 1})
+    const result = assoc(newProp, newValue, obj)
 
+    result.a // $ExpectType number
+    result.b // $ExpectType number
+  })
+  it('curried 1', () => {
+    const result = assoc(newProp, newValue)(obj)
+
+    result.a // $ExpectType number
+    result.b // $ExpectType number
+  })
+  it('curried 2', () => {
+    const result = assoc(newProp)(newValue)(obj)
+
+    result.a // $ExpectType number
     result.b // $ExpectType number
   })
 })
--- source/lens-spec.ts
+++ source/lens-spec.ts
@@ -1,4 +1,4 @@
-import {lens, assoc, lensIndex, view} from 'rambda'
+import {lens, assoc} from 'rambda'
 
 interface Dictionary<T> {
   [index: string]: T,
@@ -9,27 +9,11 @@ interface Input {
   pets: Dictionary<string>,
 }
 
-const MockObject: Input = {
-  name: 'Alice Jones',
-  address: ['22 Walnut St', 'San Francisco', 'CA'],
-  pets: {dog: 'joker', cat: 'batman'},
-}
-
-const nameLens = lens<Input, string, string>((x: Input) => {
-  return x.name
-}, assoc('name'))
-const headLens = lensIndex(0)
-
-describe('lenses', () => {
-  it('lens', () => {
-    const result = view<Input, string>(nameLens, MockObject)
-    result // $ExpectType string
-  })
-  it('lens index', () => {
-    const result = view<Input['address'], string>(
-      headLens,
-      MockObject.address
-    )
-    result // $ExpectType string
+describe('R.lens', () => {
+  it('happy', () => {
+    const fn = lens<Input, string, string>((x: Input) => {
+      return x.name
+    }, assoc('name'))
+    fn // $ExpectType Lens
   })
 })
