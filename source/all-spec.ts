35e97575e766a879fd26d4cfec74947a7f6b5ac6
Dejan Toteff <deyan8284@gmail.com>
2020-09-01 12:36:50 +0300

    feat: apply change to r.all

--- source/all-spec.ts
+++ source/all-spec.ts
@@ -11,25 +11,6 @@ describe('all', () => {
     )
     result // $ExpectType boolean
   })
-  it('pass index', () => {
-    const result = all(
-      (x, i) => {
-        x // $ExpectType number
-        i // $ExpectType number
-        return x > 0
-      },
-      [1, 2, 3]
-    )
-    result // $ExpectType boolean
-  })
-  it('pass index + curry', () => {
-    const result = all<number>((x, i) => {
-      x // $ExpectType number
-      i // $ExpectType number
-      return x > 0
-    })([1, 2, 3])
-    result // $ExpectType boolean
-  })
   it('curried needs a type', () => {
     const result = all<number>(x => {
       x // $ExpectType number
--- source/all.spec.js
+++ source/all.spec.js
@@ -1,27 +1,15 @@
 import { all } from './all'
 
-const numArr = [ 0, 1, 2, 3, 4 ]
+const list = [ 0, 1, 2, 3, 4 ]
 
 test('when true', () => {
   const fn = x => x > -1
 
-  expect(all(fn)(numArr)).toBeTrue()
+  expect(all(fn)(list)).toBeTrue()
 })
 
 test('when false', () => {
   const fn = x => x > 2
 
-  expect(all(fn, numArr)).toBeFalse()
-})
-
-test('pass index as second argument', () => {
-  const indexes = []
-  const fn = (x, i) => {
-    indexes.push(i)
-
-    return x > 5
-  }
-  all(fn, [ 10, 12, 14 ])
-
-  expect(indexes).toEqual([ 0, 1, 2 ])
+  expect(all(fn, list)).toBeFalse()
 })
