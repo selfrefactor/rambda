a568ae12de629affb87a420ae2266c6a1afbe19e
Dejan Toteff <deyan8284@gmail.com>
2020-10-23 10:42:26 +0300

    fix: r.includes typings test

--- source/includes-spec.ts
+++ source/includes-spec.ts
@@ -1,16 +1,10 @@
-import {includes} from 'ramda'
+import {includes} from 'rambda'
 
 const list = [{a: {b: '1'}}, {a: {c: '2'}}, {a: {b: '3'}}]
 
 describe('R.includes', () => {
   it('happy', () => {
     const result = includes({a: {b: '1'}}, list)
-    /*
-      should work:
-
-      const resultCurried = includes({a: {b: '1'}})(list)
-    */
-    result // $ExpectType boolean
     result // $ExpectType boolean
   })
   it('with string', () => {
