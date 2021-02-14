2f0c8ad3e3a40109f57d7c1324f338e0eec8c8a3
andrew <andrew.fountain@coredna.com>
2021-02-14 18:03:54 +1100

    chore@typings objOf removed default typing

--- files/index.d.ts
+++ files/index.d.ts
@@ -4850,26 +4850,6 @@ export function eqProps<T, U>(prop: string, obj1: T, obj2: U): boolean;
 export function eqProps<P extends string>(prop: P): <T, U>(obj1: Record<P, T>, obj2: Record<P, U>) => boolean;
 export function eqProps<T>(prop: string, obj1: T): <U>(obj2: U) => boolean;
 
-/*
-Method: objOf
-
-Explanation:
-
-Example:
-
-```
-const result = R.objOf()
-// => 
-```
-
-Categories:
-
-Notes:
-
-*/
-// @SINGLE_MARKER
-export function objOf<T>(x: T): T;
-
 // RAMBDAX_MARKER_START
 
 /*
