91f5127a9fb32ec1170b8dc73c31fe5da64c4ee9
Dejan Totev <dejan.totev@psinteractive.io>
2021-05-09 11:35:46 +0300

    test: add falsy values case for r.uniq

--- source/uniq.spec.js
+++ source/uniq.spec.js
@@ -1,8 +1,17 @@
-import { uniq } from './uniq'
+import {uniq} from './uniq'
 
 test('happy', () => {
-  expect(uniq([ 1, 2, 3, 3, 3, 1, 2, 0 ])).toEqual([ 1, 2, 3, 0 ])
-  expect(uniq([ 1, 1, 2, 1 ])).toEqual([ 1, 2 ])
-  expect([ 1, '1' ]).toEqual([ 1, '1' ])
-  expect(uniq([ [ 42 ], [ 42 ] ])).toEqual([ [ 42 ] ])
+  expect(uniq([1, 2, 3, 3, 3, 1, 2, 0])).toEqual([1, 2, 3, 0])
+})
+
+test('with nested array', () => {
+  expect(uniq([[42], [42]])).toEqual([[42]])
+})
+
+test('with falsy values', () => {
+  expect(uniq([undefined, null])).toEqual([undefined, null])
+})
+
+test('can distinct between string and number', () => {
+  expect(uniq([1, '1'])).toEqual([1, '1'])
 })
