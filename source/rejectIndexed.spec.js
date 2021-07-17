6401c58a2037f0c14f9ae0b7f0a6c930c96e9c1f
Dejan Totev <dejan.totev@psinteractive.io>
2021-07-17 16:29:10 +0300

    fix: ts test

--- source/rejectIndexed.spec.js
+++ source/rejectIndexed.spec.js
@@ -1,14 +1,13 @@
 import { rejectIndexed } from './rejectIndexed'
 
-const isOdd = (x, i) => {
-  expect(x).toBeNumber()
-  expect(i).toBeNumber()
-
-  return x % 2 === 1
-}
 
 test('with array', () => {
-  expect(rejectIndexed(isOdd)([ 1, 2, 3, 4 ])).toEqual([ 2, 4 ])
+  expect(rejectIndexed((x, i) => {
+    expect(x).toBeNumber()
+    expect(i).toBeNumber()
+  
+    return x % 2 === 1
+  })([ 1, 2, 3, 4 ])).toEqual([ 2, 4 ])
 })
 
 test('with object', () => {
@@ -18,7 +17,12 @@ test('with object', () => {
     c : 3,
     d : 4,
   }
-  expect(rejectIndexed(isOdd, obj)).toEqual({
+  expect(rejectIndexed((x, prop) => {
+    expect(x).toBeNumber()
+    expect(prop).toBeString()
+  
+    return x % 2 === 1
+  }, obj)).toEqual({
     b : 2,
     d : 4,
   })
