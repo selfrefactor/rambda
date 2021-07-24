1a87e478f14fc9f08d55b845e54e88ae427e21f7
Dejan Totev <dejan.totev@psinteractive.io>
2021-07-24 15:21:54 +0300

    feat: r.filter.object tested typings

---
+++ source/filterObject-spec.ts
@@ -0,0 +1,26 @@
+import {filterObject} from 'rambda'
+
+const obj = {a: 1, b: 2}
+
+describe('R.filterObject', () => {
+  it('happy', () => {
+    const result = filterObject((val, prop, origin) => {
+      val // $ExpectType number
+      prop // $ExpectType string
+      origin // $ExpectType Dictionary<number>
+
+      return val > 1
+    }, obj)
+    result // $ExpectType Dictionary<number>
+  })
+  it('curried version requires explicit type', () => {
+    const result = filterObject<number>((val, prop, origin) => {
+      val // $ExpectType number
+      prop // $ExpectType string
+      origin // $ExpectType Dictionary<number>
+
+      return val > 1
+    })(obj)
+    result // $ExpectType Dictionary<number>
+  })
+})
