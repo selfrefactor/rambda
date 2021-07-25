b7ff7aff64d4906e94aa2885bc5a8a78324f4475
Dejan Totev <dejan.totev@psinteractive.io>
2021-07-25 17:08:44 +0300

    fix@test r.without

--- source/without.js
+++ source/without.js
@@ -1,16 +1,14 @@
-import { reduce } from './reduce'
-import { _indexOf } from './_internals/_indexOf'
+import {reduce} from './reduce'
+import {_indexOf} from './_internals/_indexOf'
 
-export function without(matchAgainst, source){
-  if (source === undefined){
+export function without(matchAgainst, source) {
+  if (source === undefined) {
     return _source => without(matchAgainst, _source)
   }
 
   return reduce(
-    (prev, current) =>{
-      console.log(prev, current, matchAgainst, '||')
-      return _indexOf(current, matchAgainst) > -1 ? prev : prev.concat(current)
-    },
+    (prev, current) =>
+      _indexOf(current, matchAgainst) > -1 ? prev : prev.concat(current),
     [],
     source
   )
--- source/without.spec.js
+++ source/without.spec.js
@@ -1,29 +1,28 @@
-import { without } from './without'
+import {without} from './without'
 import {without as ramdaWithout} from 'ramda'
 
 test('should return a new list without values in the first argument', () => {
-  const itemsToOmit = [ 'A', 'B', 'C' ]
-  const collection = [ 'A', 'B', 'C', 'D', 'E', 'F' ]
+  const itemsToOmit = ['A', 'B', 'C']
+  const collection = ['A', 'B', 'C', 'D', 'E', 'F']
 
-  expect(without(itemsToOmit, collection)).toEqual([ 'D', 'E', 'F' ])
-  expect(without(itemsToOmit)(collection)).toEqual([ 'D', 'E', 'F' ])
+  expect(without(itemsToOmit, collection)).toEqual(['D', 'E', 'F'])
+  expect(without(itemsToOmit)(collection)).toEqual(['D', 'E', 'F'])
 })
 
-test.only('with list of objects', () => {
-  const itemsToOmit = [ {a: 1}, {c: 3} ]
-  const collection = [{a: 1}, {b: 2}, {c: 3}, {d: 4} ]
-  const expected = [ {b: 2}, {d: 4} ]
+test('with list of objects', () => {
+  const itemsToOmit = [{a: 1}, {c: 3}]
+  const collection = [{a: 1}, {b: 2}, {c: 3}, {d: 4}]
+  const expected = [{b: 2}, {d: 4}]
 
   expect(without(itemsToOmit, collection)).toEqual(expected)
   expect(ramdaWithout(itemsToOmit, collection)).toEqual(expected)
 })
 
 test('ramda bug', () => {
-  expect(
-    without("0:1", ["0", "0:1"])
-  ).toEqual(['0:1'])
+  expect(without('0:1', ['0', '0:1'])).toEqual([])
+  expect(without(['0:1'], ['0', '0:1'])).toEqual(['0'])
 })
 
 test('ramda test', () => {
-  expect(without([ 1, 2 ])([ 1, 2, 1, 3, 4 ])).toEqual([ 3, 4 ])
+  expect(without([1, 2])([1, 2, 1, 3, 4])).toEqual([3, 4])
 })
