a6935e87ea1301e689feac73d85c84eb95c5a957
Dejan Totev <dejan.totev@psinteractive.io>
2021-07-25 10:13:21 +0300

    fix: test

--- source/partitionIndexed.spec.js
+++ source/partitionIndexed.spec.js
@@ -8,10 +8,12 @@ test('with array', () => {
     const list = [ 1, 2, 3, 4 ]
 
   const result = partitionIndexed(predicate, list)
+  const curried = partitionIndexed(predicate)(list)
   const expectedResult = [
     [ 3, 4 ],
     [ 1, 2 ],
   ]
 
   expect(result).toEqual(expectedResult)
+  expect(curried).toEqual(expectedResult)
 })
