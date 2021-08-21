c6b7825ba131f1552432f41b6bb3540d5cd277ee
Dejan Totev <dejan.totev@psinteractive.io>
2021-08-21 14:57:08 +0300

    test: r.update with negative index

--- source/update.spec.js
+++ source/update.spec.js
@@ -1,17 +1,15 @@
-import { update } from './update'
+import {update} from './update'
 
-const list = [ 1, 2, 3 ]
+const list = [1, 2, 3]
 
 test('happy', () => {
   const newValue = 8
   const index = 1
-  const result = update(
-    index, newValue, list
-  )
+  const result = update(index, newValue, list)
   const curriedResult = update(index, newValue)(list)
   const tripleCurriedResult = update(index)(newValue)(list)
 
-  const expected = [ 1, 8, 3 ]
+  const expected = [1, 8, 3]
   expect(result).toEqual(expected)
   expect(curriedResult).toEqual(expected)
   expect(tripleCurriedResult).toEqual(expected)
@@ -20,9 +18,13 @@ test('happy', () => {
 test('list has no such index', () => {
   const newValue = 8
   const index = 10
-  const result = update(
-    index, newValue, list
-  )
+  const result = update(index, newValue, list)
 
   expect(result).toEqual(list)
+})
+
+test('with negative index', () => {
+  expect(
+    update(-1, 10, list)
+  ).toEqual([1,2,10])
 })
