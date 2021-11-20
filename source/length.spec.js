6a6effa55d0f296961aef0d87113f8914a1413ad
Dejan Totev <dejan.totev@psinteractive.io>
2021-11-20 13:04:54 +0200

    fix: r.length

--- source/length.spec.js
+++ source/length.spec.js
@@ -1,52 +1,32 @@
-import { length } from "./length";
-import { length as lengthRamda } from "ramda";
-import { compareCombinations } from "./_internals/testUtils";
+import {length} from './length'
+import {length as lengthRamda} from 'ramda'
 
-test("happy", () => {
-  expect(length("foo")).toEqual(3);
-  expect(length([1, 2, 3])).toEqual(3);
-  expect(length([])).toEqual(0);
-});
+test('happy', () => {
+  expect(length('foo')).toEqual(3)
+  expect(length([1, 2, 3])).toEqual(3)
+  expect(length([])).toEqual(0)
+})
 
-test("with empty string", () => {
-  expect(length("")).toEqual(0);
-});
+test('with empty string', () => {
+  expect(length('')).toEqual(0)
+})
 
-test("with bad input returns NaN", () => {
-  expect(length(0)).toBeNaN();
-  expect(lengthRamda(0)).toBeNaN();
-  expect(length({})).toBeNaN();
-  expect(length(null)).toBeNaN();
-  expect(length(undefined)).toBeNaN();
-});
+test('with bad input returns NaN', () => {
+  expect(length(0)).toBeNaN()
+  expect(length({})).toBeNaN()
+  expect(length(null)).toBeNaN()
+  expect(length(undefined)).toBeNaN()
+})
 
-const possibleInputs = [
-  NaN,
-  [/foo/],
-  /foo/,
-  {},
-  null,
-  undefined,
-  0,
-  Promise.resolve(1),
-];
+test('with length as property', () => {
+  const input1 = {length: '123'}
+  const input2 = {length: null}
+  const input3 = {length: ''}
 
-describe("brute force", () => {
-  compareCombinations({
-    fn: length,
-    fnRamda: lengthRamda,
-    firstInput: possibleInputs,
-    callback: (errorsCounters) => {
-      expect(errorsCounters).toMatchInlineSnapshot(`
-        Object {
-          "ERRORS_MESSAGE_MISMATCH": 0,
-          "ERRORS_TYPE_MISMATCH": 0,
-          "RESULTS_MISMATCH": 0,
-          "SHOULD_NOT_THROW": 0,
-          "SHOULD_THROW": 0,
-          "TOTAL_TESTS": 8,
-        }
-      `);
-    },
-  });
-});
+  expect(length(input1)).toBeNaN()
+  expect(lengthRamda(input1)).toBeNaN()
+  expect(length(input2)).toBeNaN()
+  expect(lengthRamda(input2)).toBeNaN()
+  expect(length(input3)).toBeNaN()
+  expect(lengthRamda(input3)).toBeNaN()
+})
