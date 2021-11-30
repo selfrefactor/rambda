f13b16910bb02f185743f61de5e0c28c068b7624
Dejan Totev <dejan.totev@psinteractive.io>
2021-11-30 14:27:48 +0200

    chore@dnd

--- source/groupBy-spec.ts
+++ source/groupBy-spec.ts
@@ -1,6 +1,23 @@
 import {groupBy} from 'rambda'
 import {groupBy as groupByRamda, prop} from 'ramda'
 
+interface Thing {
+  name: string;
+  position: string;
+}
+
+interface Output {
+  left: Thing[];
+  right: Thing[];
+}
+
+const things = [
+  { name: 'one', position: 'left' },
+  { name: 'two', position: 'left' },
+  { name: 'three', position: 'right' },
+  { name: 'four', position: 'right' },
+]
+
 describe('R.groupBy', () => {
   it('happy', () => {
     const groupByFn = (x: string) => String(x.length)
@@ -12,33 +29,21 @@ describe('R.groupBy', () => {
     const curriedResult = groupBy(groupByFn)(list)
     curriedResult // $ExpectType { [index: string]: string[]; }
   })
-  it('issue test', () => {
-    interface Thing {
-      name: string;
-      position: string;
-    }
-    
-    interface Output {
-      left: Thing[];
-      right: Thing[];
-    }
-    
-    const things = [
-      { name: 'one', position: 'left' },
-      { name: 'two', position: 'left' },
-      { name: 'three', position: 'right' },
-      { name: 'four', position: 'right' },
-    ]
-    
-    const groupByPosition = groupBy<Thing, Output>(prop('position'));
-    const groupByPositionX = groupBy<Thing>(prop('position'));
+  it('with one explicit types', () => {    
+    const groupByPosition = groupBy<Thing>(prop('position'));
 
     // $ExpectError
     groupByRamda(prop('position'));
     
     const result = groupByPosition(things)
-    const resultx = groupByPositionX(things)
+    result // $ExpectType { [index: string]: Thing[]; }
+    result[9]  // $ExpectType Thing[]
+    result.foo  // $ExpectType Thing[]
+  })
+  it('with two explicit types', () => {    
+    const groupByPosition = groupBy<Thing, Output>(prop('position'));
+    
+    const result = groupByPosition(things)
     result // $ExpectType Output
-    resultx // $ExpectType { [index: string]: Thing[]; }
   })
 })
