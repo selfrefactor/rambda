226d81590c04fb555c422d4c46af081455716e16
Dejan Totev <dejan.totev@psinteractive.io>
2021-12-11 14:06:34 +0200

    feat@important export types and babel config rename

--- source/eqProps.spec.js
+++ source/eqProps.spec.js
@@ -1,36 +1,51 @@
-import {eqProps as eqPropsRamda} from 'ramda'
+import { eqProps as eqPropsRamda } from "ramda";
 
-import {compareCombinations} from './_internals/testUtils'
-import {eqProps} from './eqProps'
+import { compareCombinations } from "./_internals/testUtils";
+import { eqProps } from "./eqProps";
 
 const obj1 = {
   a: 1,
   b: 2,
-}
+};
 const obj2 = {
   a: 1,
   b: 3,
-}
+};
 
-test('props are equal', () => {
-  const result = eqProps('a', obj1, obj2)
-  expect(result).toBeTrue()
-})
+test("props are equal", () => {
+  const result = eqProps("a", obj1, obj2);
+  expect(result).toBeTrue();
+});
 
-test('props are not equal', () => {
-  const result = eqProps('b', obj1, obj2)
-  expect(result).toBeFalse()
-})
+test("props are not equal", () => {
+  const result = eqProps("b", obj1, obj2);
+  expect(result).toBeFalse();
+});
 
-test('prop does not exist ', () => {
-  const result = eqProps('c', obj1, obj2)
-  expect(result).toBeTrue()
-})
+test("prop does not exist ", () => {
+  const result = eqProps("c", obj1, obj2);
+  expect(result).toBeTrue();
+});
 
-const possibleProps = ['a', 'a.b', null, false, 0, 1, {}, []]
+test("can handle null or undefined object", function () {
+  expect(eqProps("value", { value: 0 }, null)).toEqual(false);
+  expect(eqProps("value", { value: 0 }, undefined)).toEqual(false);
+  expect(eqProps("value", null, { value: 0 })).toEqual(false);
+  expect(eqProps("value", undefined, { value: 0 })).toEqual(false);
+  expect(eqProps("value", undefined, { value: undefined })).toEqual(true);
+  expect(eqProps("value", null, { value: undefined })).toEqual(true);
+  expect(eqProps("value", { value: undefined }, undefined)).toEqual(true);
+  expect(eqProps("value", { value: undefined }, null)).toEqual(true);
+  expect(eqProps("value", {}, null)).toEqual(true);
+  expect(eqProps("value", {}, undefined)).toEqual(true);
+  expect(eqProps("value", null, {})).toEqual(true);
+  expect(eqProps("value", undefined, {})).toEqual(true);
+});
+
+const possibleProps = ["a", "a.b", null, false, 0, 1, {}, []];
 
 const possibleObjects = [
-  {a: 1},
+  { a: 1 },
   {
     a: 1,
     b: 2,
@@ -39,39 +54,37 @@ const possibleObjects = [
   [],
   null,
   {
-    a: {b: 1},
+    a: { b: 1 },
     c: 2,
   },
   {
-    a: {b: 1},
+    a: { b: 1 },
     c: 3,
   },
-  {a: {b: 2}},
-]
+  { a: { b: 2 } },
+];
 
-describe('brute force', () => {
-  let totalTestsCounter = 0
+describe("brute force", () => {
+  let totalTestsCounter = 0;
 
   compareCombinations({
     firstInput: possibleProps,
     setCounter: () => totalTestsCounter++,
-    callback: errorsCounters => {
-      // console.log({ totalTestsCounter })
-
+    callback: (errorsCounters) => {
       expect(errorsCounters).toMatchInlineSnapshot(`
         Object {
           "ERRORS_MESSAGE_MISMATCH": 0,
-          "ERRORS_TYPE_MISMATCH": 120,
+          "ERRORS_TYPE_MISMATCH": 0,
           "RESULTS_MISMATCH": 0,
           "SHOULD_NOT_THROW": 0,
-          "SHOULD_THROW": 0,
+          "SHOULD_THROW": 120,
           "TOTAL_TESTS": 512,
         }
-      `)
+      `);
     },
     secondInput: possibleObjects,
     thirdInput: possibleObjects,
     fn: eqProps,
     fnRamda: eqPropsRamda,
-  })
-})
+  });
+});
