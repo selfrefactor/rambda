15982c6551793d77ff2455e597e95a94fb8afcdd
Dejan Toteff <deyan8284@gmail.com>
2022-02-04 22:45:19 +0000

    feat: r.flatten.object is ready

--- source/flattenObject.js
+++ source/flattenObject.js
@@ -1,7 +1,70 @@
-export function flattenObject(foo, bar) {
-  if (arguments.length === 1){
-    return (_bar) => flattenObject(foo, _bar);
+import { type } from "./type"
+
+export function flattenObjectHelper(obj, accumulator = []) {
+      let willReturn = {}
+  Object.keys(obj).forEach(key => {
+    const typeIs = type(obj[key])
+    if (typeIs === 'Object') {
+      const [flatResultValue, flatResultPath] = flattenObjectHelper(obj[key], [...accumulator, key])
+      willReturn[flatResultPath.join('.')] = flatResultValue
+      return
+    }else if(accumulator.length > 0){
+      const finalKey = [...accumulator, key].join('.')
+      willReturn[finalKey] = obj[key]
+      return
+    }
+    willReturn[key] = obj[key]
+  })
+  if(accumulator.length > 0) return [willReturn, accumulator]
+  return willReturn
+}
+
+export function transformFlatObject(obj){
+  const willReturn = {};
+
+  const transformFlatObjectFn = (objLocal) => {
+    const willReturnLocal = {}
+    Object.keys(objLocal).forEach(key => {
+      const typeIs = type(objLocal[key])
+      if (typeIs === 'Object') {
+        transformFlatObjectFn(objLocal[key])
+        return
+      }
+      willReturnLocal[key] = objLocal[key]
+      willReturn[key] = objLocal[key]
+    })
+  
+    return willReturnLocal
   }
 
-  return
+  Object.keys(obj).forEach(key => {
+    const typeIs = type(obj[key])
+    if (typeIs === 'Object') {
+      transformFlatObjectFn(obj[key], key)
+      return
+    }
+    willReturn[key] = obj[key]
+  })
+
+  return willReturn
+}
+
+export function flattenObject(obj) {
+  let willReturn = {}
+
+  Object.keys(obj).forEach(key => {
+    const typeIs = type(obj[key])
+    if (typeIs === 'Object') {
+      const flatObject = flattenObjectHelper(obj[key])
+      const transformed  = transformFlatObject(flatObject)
+
+      Object.keys(transformed).forEach(keyTransformed => {
+        willReturn[`${key}.${keyTransformed}`] = transformed[keyTransformed]
+      })
+    }else{
+      willReturn[key] = obj[key]
+    }
+  })
+
+  return willReturn
 }
