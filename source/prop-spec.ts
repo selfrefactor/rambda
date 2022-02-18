60fde495076905d20db3dd45f95501c40c97dc3d
Hlib Hraif <hlib.hraif@protonmail.com>
2022-02-18 23:49:24 +0100

    fix: Add support for object type in curried prop function

--- source/prop-spec.ts
+++ source/prop-spec.ts
@@ -1,8 +1,9 @@
-import {prop} from 'rambda'
-
-const obj = {a: 1, b: 'foo'}
+import {pipe, prop} from 'rambda'
 
 describe('R.prop', () => {
+  const obj = {a: 1, b: 'foo'}
+  type Something = {a?: number, b?: string}
+
   it('issue #553', () => {
     const result = prop('e', {e: 'test1', d: 'test2'})
     const curriedResult = prop<string>('e')({e: 'test1', d: 'test2'})
@@ -17,6 +18,21 @@ describe('R.prop', () => {
   })
   it('curried', () => {
     const result = prop('b')(obj)
+
+    result // $ExpectType string
+  })
+  it('curried with explicit object type', () => {
+    const result = prop<'a', Something>('a')(obj)
+
+    result // $ExpectType number | undefined
+  })
+  it('curried with implicit object type', () => {
+    const result = pipe((value) => value as Something, prop('b'))(obj)
+
+    result // $ExpectType string | undefined
+  })
+  it('curried with explicit result type', () => {
+    const result = prop<'b', string>('b')(obj)
 
     result // $ExpectType string
   })
