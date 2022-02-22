e806ce752aa6e2ab28010e3c18aa75a81aceb92f
Dejan Toteff <deyan8284@gmail.com>
2022-02-22 08:29:02 +0200

    fix: wrong r.none (#626)

--- source/none.spec.js
+++ source/none.spec.js
@@ -1,13 +1,11 @@
 import {none} from './none'
 
 const isEven = n => n % 2 === 0
-const isOdd = n => n % 2 === 1
-const arr = [1, 3, 5, 7, 9, 11]
 
 test('when true', () => {
-  expect(none(isEven, arr)).toBeTrue()
+  expect(none(isEven, [1, 3, 5, 7])).toBeTrue()
 })
 
 test('when false curried', () => {
-  expect(none(isOdd)(arr)).toBeFalse()
+  expect(none(isOdd)([1, 3, 5, 8])).toBeFalse()
 })
