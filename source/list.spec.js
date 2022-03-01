b3dc49ace161542bba9edc47ead896bf3c289ec9
Dejan Toteff <deyan8284@gmail.com>
2022-03-01 14:34:40 +0000

    chore@dnd

--- source/list.spec.js
+++
@@ -1,9 +0,0 @@
-import { List } from './list'
-
-test('happy', () => {
-  const list = new List(1,2,3,4,5,6,7,8,9)
-  console.log(list)
-  // list.push(2)
-  const chunk1 = list.slice("0:3")
-  console.log(`chunk1`, chunk1)
-})
