a37af0d605fcfb64d75f27810c4fc9fd9ad8e85e
Dejan Toteff <deyan8284@gmail.com>
2022-04-05 20:55:22 +0000

    feat: juxt

--- source/juxt.spec.js
+++ source/juxt.spec.js
@@ -1,9 +1,7 @@
-import { juxt } from './juxt.js'
+import {juxt} from './juxt.js'
 
 test('happy', () => {
-  const getRange = juxt([ Math.min, Math.max ])
-  const result = getRange(
-    3, 4, 9, -3
-  )
-  expect(result).toEqual([ -3, 9 ])
+  const getRange = juxt([Math.min, Math.max, Math.min])
+  const result = getRange(3, 4, 9, -3)
+  expect(result).toEqual([-3, 9, -3])
 })
