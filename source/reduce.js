07e2a4cf698295b86d3b5f9a9751d1610f872098
Aron Griffis <aron@scampersand.com>
2022-04-06 10:51:41 -0400

    feat: reduced

--- source/reduce.js
+++ source/reduce.js
@@ -2,6 +2,12 @@ import {_isArray} from './_internals/_isArray'
 import {_keys} from './_internals/_keys'
 import {curry} from './curry'
 
+class Reduced {
+  constructor(value) {
+    this.value = value
+  }
+}
+
 function reduceFn(reducer, acc, list) {
   if (!_isArray(list)) {
     throw new TypeError('reduce: list must be array or iterable')
@@ -11,6 +17,9 @@ function reduceFn(reducer, acc, list) {
 
   while (index < len) {
     acc = reducer(acc, list[index], index, list)
+    if (acc instanceof Reduced) {
+      return acc.value
+    }
     index++
   }
 
@@ -18,3 +27,5 @@ function reduceFn(reducer, acc, list) {
 }
 
 export const reduce = curry(reduceFn)
+
+export const reduced = value => new Reduced(value)
--- source/reduce.spec.js
+++ source/reduce.spec.js
@@ -1,4 +1,4 @@
-import {reduce} from './reduce'
+import {reduce, reduced} from './reduce'
 
 const reducer = (prev, current, i) => {
   expect(i).toBeNumber()
@@ -24,4 +24,26 @@ test('with object as iterable', () => {
 
 test('with undefined as iterable', () => {
   expect(() => reduce(reducer, 0, null)).toThrowWithMessage(TypeError, ERROR)
+})
+
+describe('reduced', () => {
+  test('without reduced (baseline)', () => {
+    let maxIndex
+    const reducer = (prev, current, i) => {
+      maxIndex = i
+      return current === 2 ? current : prev
+    }
+    expect(reduce(reducer, initialValue, list)).toEqual(2)
+    expect(maxIndex).toEqual(2) // did not stop early
+  })
+
+  test('with reduced', () => {
+    let maxIndex
+    const reducer = (prev, current, i) => {
+      maxIndex = i
+      return current === 2 ? reduced(current) : prev
+    }
+    expect(reduce(reducer, initialValue, list)).toEqual(2)
+    expect(maxIndex).toEqual(1) // stopped early
+  })
 })
