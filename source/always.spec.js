11a69ba1763e3926216ad96f863f2c33b81de4b3
Dejan Toteff <deyan8284@gmail.com>
2022-05-04 13:27:54 +0000

    fix: r.always

--- source/always.spec.js
+++ source/always.spec.js
@@ -1,16 +1,14 @@
 import { always } from './always.js'
-import { F } from './F.js'
+import { applySpec } from './applySpec.js'
 
 test('happy', () => {
   const fn = always(7)
 
-  expect(fn()).toEqual(7)
-  expect(fn()).toEqual(7)
+  expect(fn()).toBe(7)
+  expect(fn()).toBe(7)
 })
 
-test('f', () => {
-  const fn = always(F())
-
-  expect(fn()).toBeFalse()
-  expect(fn()).toBeFalse()
+test('compatibility with applySpec', () => {
+  const spec = applySpec({ x : always('foo') })
+  expect(spec({})).toEqual({ x : 'foo' })
 })
