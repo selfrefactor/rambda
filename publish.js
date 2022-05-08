5b4dceb69d09886f1c5f28bab3c4868d415a1d22
Dejan Toteff <deyan8284@gmail.com>
2022-05-08 16:41:48 +0000

    fix@issue #633 wrong deno import

---
+++ publish.js
@@ -0,0 +1,19 @@
+const { exec } = require('helpers-fn')
+const [,,publishType] = process.argv
+
+const cwd = __dirname
+
+void async function publish(){
+  console.log(`publishType`, publishType)
+  if(!['minor', 'major', 'patch'].includes(publishType)){
+    throw new Error(`wrong publishType - ${publishType}`)
+  }
+
+  await exec({cwd, command: `run bump ${publishType}`})
+  await exec({cwd, command: `eggs publish --release-type ${publishType} --yes`})
+  await exec({cwd, command: `eggs publish --release-type ${publishType} --yes`})
+  await exec({cwd, command: `yarn github`})
+  await exec({cwd, command: `yarn docs`})
+  await exec({cwd, command: `run d ${publishType} release`})
+  console.log(a)
+}()
