87f307150961ceee39963161245ca1c590236599
Dejan Toteff <deyan8284@gmail.com>
2022-06-25 10:02:17 +0000

    chore@small

--- source/propEq.js
+++ source/propEq.js
@@ -1,11 +1,13 @@
 import { curry } from './curry.js'
+import equals from './equals.js'
+import prop from './prop.js'
 
 function propEqFn(
   propToFind, valueToMatch, obj
 ){
   if (!obj) return false
 
-  return obj[ propToFind ] === valueToMatch
+  return equals(valueToMatch, prop(propToFind, obj))
 }
 
 export const propEq = curry(propEqFn)
