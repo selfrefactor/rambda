52cf2e7614416b5aee323419bda16bd46eb4e644
Dejan Toteff <deyan8284@gmail.com>
2022-06-25 08:44:56 +0000

    chore@small

--- source/uniqBy.spec.js
+++ source/uniqBy.spec.js
@@ -1,16 +1,13 @@
 import { uniqBy } from './uniqBy.js'
 
-test('returns a set from any array based on predicate', () => {
-  const result = uniqBy(Math.abs, [ -2, -1, 0, 1, 2 ])
-  console.log(result, 'result')
+test('happy', () => {
+  expect(uniqBy(Math.abs, [ -2, -1, 0, 1, 2 ])).toEqual([ -2, -1, 0 ])
 })
 
 test('keeps elements from the left', () => {
-  const result = uniqBy(Math.abs, [ -1, 2, 4, 3, 1, 3 ])
-  console.log(result, 'result')
+  expect(uniqBy(Math.abs, [ -1, 2, 4, 3, 1, 3 ])).toEqual([ -1, 2, 4, 3 ])
 })
 
 test('returns an empty array for an empty array', () => {
-  const result = uniqBy(Math.abs, [])
-  console.log(result, 'result')
+  expect(uniqBy(Math.abs, [])).toEqual([])
 })
