d19ff2dd9b9b15d7b9ae99d4223a507acc8bc69c
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-05 10:02:51 +0300

    feat: now expect error

--- files/index.d.ts
+++ files/index.d.ts
@@ -3280,7 +3280,10 @@ export function propIs<C extends AnyFunction>(type: C): {
     <K extends keyof any>(name: K, obj: any): obj is Record<K, ReturnType<C>>;
     <K extends keyof any>(name: K): (obj: any) => obj is Record<K, ReturnType<C>>;
 };
-
+export function propIs<C extends AnyFunction>(type: C): {
+  <K extends keyof any>(name: K, obj: any): obj is Record<K, InstanceType<C>>;
+  <K extends keyof any>(name: K): (obj: any) => obj is Record<K, InstanceType<C>>;
+}
 
 /*
 Method: propOr
