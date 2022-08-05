c814759f564910e5dd6378e8966140e0da79a9f2
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-05 12:40:37 +0300

    feat@expect fixed

--- files/index.d.ts
+++ files/index.d.ts
@@ -3280,10 +3280,6 @@ export function propIs<C extends AnyFunction>(type: C): {
     <K extends keyof any>(name: K, obj: any): obj is Record<K, ReturnType<C>>;
     <K extends keyof any>(name: K): (obj: any) => obj is Record<K, ReturnType<C>>;
 };
-export function propIs<C extends AnyFunction>(type: C): {
-  <K extends keyof any>(name: K, obj: any): obj is Record<K, InstanceType<C>>;
-  <K extends keyof any>(name: K): (obj: any) => obj is Record<K, InstanceType<C>>;
-}
 
 /*
 Method: propOr
