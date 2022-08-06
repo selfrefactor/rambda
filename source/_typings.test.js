7b1aa0a3d2ce1e99e8da869a3ced220410ea05ed
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-06 12:39:51 +0300

    chore@small

--- source/_typings.test.js
+++ source/_typings.test.js
@@ -1,17 +1,7 @@
-import { spawnSync } from 'child_process'
-import { resolve } from 'path'
+import { execCommand } from '../files/execCommand'
 
 jest.setTimeout(3 * 60 * 1000)
 
-const DIR = resolve(__dirname, '../')
-
 test('typings are correct', async () => {
-  const { status } = spawnSync(
-    'yarn', [ 'typings' ], {
-      stdio : 'inherit',
-      cwd   : DIR,
-    }
-  )
-
-  expect(status).toBe(0)
+  expect(await execCommand(`yarn consume-typings`)).toBeTrue()  
 })
