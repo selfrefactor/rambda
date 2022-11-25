1493373594f1720dd3374b3ba19a5e627ed8f7f9
Deyan Totev <deyan.totev.extern@seven.one>
2022-11-25 17:52:10 +0200

    feat: compare size of output

---
+++ files/rollup.config.ramda.mjs
@@ -0,0 +1,30 @@
+import babel from '@rollup/plugin-babel';
+import cleanup from 'rollup-plugin-cleanup'
+import replace from '@rollup/plugin-replace'
+import {nodeResolve}  from '@rollup/plugin-node-resolve'
+
+const extensions = [ '.js' ]
+
+export default {
+  plugins : [
+    replace({ preventAssignment: true, 'process.env.NODE_ENV' : JSON.stringify('production') }),
+    nodeResolve({
+      extensions,
+      browser: false,
+      preferBuiltins : true,
+    }),
+    cleanup(),
+    babel({ 
+      babelHelpers: 'bundled',
+        extensions,
+        exclude : [ 'node_modules/**' ],
+    })
+  ],
+  input  : 'compare-size.js',
+  output : [
+    {
+      file   : './compare-size-output.js',
+      format : 'cjs',
+    },
+  ],
+}
