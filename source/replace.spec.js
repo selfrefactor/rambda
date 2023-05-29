6a3f0e4089ac27141d930637866c295147bef281
Deyan Totev <deyan.totev.extern@seven.one>
2023-05-29 15:49:49 +0300

    r.replace

--- source/replace.spec.js
+++ source/replace.spec.js
@@ -1,5 +1,14 @@
 import { replace } from './replace.js'
 
 test('happy', () => {
-  expect(replace(/\s/g)('|')('foo bar baz')).toBe('foo|bar|baz')
+  expect(replace(/\s/g, '|', 'foo bar baz')).toBe('foo|bar|baz')
+})
+
+test('with function as replacer input', () => {
+  expect(replace(/\s/g, (match,offset,str) => {
+    expect(match).toBe(' ')
+    expect([3,7].includes(offset)).toBeTrue()
+    expect(str).toBe('foo bar baz')
+    return '|'
+  }, 'foo bar baz')).toBe('foo|bar|baz')
 })
