e40782b4b5f9698737d1307a0863c849ab7b7126
Deyan Totev <deyan.totev.extern@seven.one>
2023-05-29 12:00:46 +0300

    chore@small

--- source/replace.spec.js
+++ source/replace.spec.js
@@ -1,19 +1,5 @@
 import { replace } from './replace.js'
 
 test('happy', () => {
-  expect(replace(
-    'foo', 'yes', 'foo bar baz'
-  )).toBe('yes bar baz')
-})
-
-test('1', () => {
   expect(replace(/\s/g)('|')('foo bar baz')).toBe('foo|bar|baz')
-})
-
-test('2', () => {
-  expect(replace(/\s/g)('|', 'foo bar baz')).toBe('foo|bar|baz')
-})
-
-test('3', () => {
-  expect(replace(/\s/g, '|')('foo bar baz')).toBe('foo|bar|baz')
 })
