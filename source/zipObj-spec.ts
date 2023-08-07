3f6d35f1b047584150bd0b9a5a063481fb44f84a
Petra Jaros <peeja@peeja.com>
2023-08-07 15:27:54 -0400

    `R.prepend`/`R.append` fns accept broader types (#699)
    * `R.prepend`/`R.append` fns accept broader types
    
    Before, `R.prepend(4)` or `R.append(4)` would produce a function which
    could only be applied to a `number[]`. However, you may well want to
    append a `4` to an array whose elements can be `number`s or other
    things, such as a `(string | number)[]`. This commit improves the types
    to accept such arrays.
    
    You may also have an array of a more specific type (such as `number[]`)
    and wish to use `R.prepend`/`R.append` to expand the type. Because it's
    usually a mistake, `append("d")(listOfNumbers)` will cause a type error;
    however, explicitly calling `append("d")<string |
    number>(listOfNumbers)` will give you a `(string | number)[]`.
    
    * Update order of `R.zipObj` expected type
    
    Not clear why this changed. Ideally, the test runner wouldn't care about
    the order of keys in an object type.

--- source/zipObj-spec.ts
+++ source/zipObj-spec.ts
@@ -4,7 +4,7 @@ describe('R.zipObj', () => {
   it('happy', () => {
     // this is wrong since 24.10.2020 `@types/ramda` changes
     const result = zipObj(['a', 'b', 'c', 'd'], [1, 2, 3])
-    result // $ExpectType { b: number; a: number; c: number; d: number; }
+    result // $ExpectType { b: number; d: number; a: number; c: number; }
   })
   it('imported from @types/ramda', () => {
     const result = zipObj(['a', 'b', 'c'], [1, 2, 3])
