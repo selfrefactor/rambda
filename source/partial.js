6fd40fd0282a1b4aa51c99d9619184560b81d978
Petra Jaros <peeja@peeja.com>
2023-08-08 19:40:21 -0400

    Fix `R.partial` (#701)
    * **Implementation:** The documentation states that Rambda's partial can
      either take arguments as an array (like Ramda) or as variadic
      arguments. However, the implementation didn't actually support the
      Ramda style. Now it does.
    
    * **Tests:** The tests made it look as if the Ramda style worked.
      However, this was only because we were concatenating the results with
      `+`, and `"a" + ["b"] === "a" + "b"`. The tests now truly validate
      that Ramda style works.
    
    * **Types:** Meanwhile, the types *only* permitted the Ramda style, even
      though that style didn't work. I've replaced those with a new type
      which a) supports the variadic style, and b) doesn't rely on a finite
      set of overrides. There are now only two overrides: one for the
      variadic style and one for the Ramda style.

--- source/partial.js
+++ source/partial.js
@@ -1,11 +1,17 @@
+import { isArray } from './_internals/isArray.js'
+
 export function partial(fn, ...args){
   const len = fn.length
 
+  // If a single array argument is given, those are the args (a la Ramda).
+  // Otherwise, the variadic arguments are the args.
+  const argList = args.length === 1 && isArray(args[0]) ? args[0] : args
+
   return (...rest) => {
-    if (args.length + rest.length >= len){
-      return fn(...args, ...rest)
+    if (argList.length + rest.length >= len){
+      return fn(...argList, ...rest)
     }
 
-    return partial(fn, ...[ ...args, ...rest ])
+    return partial(fn, ...[ ...argList, ...rest ])
   }
 }
