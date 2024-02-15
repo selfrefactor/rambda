e0fff9f366b2e35e2d777f49514b35448aa476fe
Hendrik Belitz <hendrik@innovation-through-understanding.de>
2024-02-15 18:26:17 +0100

    Updated bundles

--- dist/rambda.js
+++ dist/rambda.js
@@ -1535,7 +1535,6 @@ function isEmpty(input) {
   const inputType = type(input);
   if (['Undefined', 'NaN', 'Number', 'Null'].includes(inputType)) return false;
   if (!input) return true;
-  console.error(type(input.isEmpty), input.isEmpty);
   if (type(input.isEmpty) === 'Function') {
     return input.isEmpty();
   } else if (input.isEmpty) {
