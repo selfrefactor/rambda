b312bd09ae1c8b2955b988266665c655e2fb4b54
Deyan Totev <deyan8282@gmail.com>
2024-04-02 11:29:10 +0300

    fix: wrong ts

--- files/index.d.ts
+++ files/index.d.ts
@@ -7003,9 +7003,8 @@ Notes:
 // @SINGLE_MARKER
 export function mapToObjectAsync<T, U extends object>(fn: (input: T) => Promise<U|false>, list: readonly T[]): Promise<U>;
 export function mapToObjectAsync<T, U extends object>(fn: (input: T) => Promise<U|false>): (list: readonly T[]) => Promise<U>;
-export function mapToObjectAsync<T, U>(fn: (input: T) => Promise<Output[keyof Output]|false>, list: T[]): Promise<U>;
-export function mapToObjectAsync<T, U>(fn: (input: T) => Promise<Output[keyof Output]|false>): (list: T[]) => Promise<U>;
-
+export function mapToObjectAsync<T, U>(fn: (input: T) => object|false, list: T[]): U;
+export function mapToObjectAsync<T, U>(fn: (input: T) => object|false): (list: T[]) => U;
 /*
 Method: mapKeys
 
