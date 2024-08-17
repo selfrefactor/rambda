2d2b856cde890f4828bdd868f4ce4bc62ef9f1c0
Deyan Totev <deyan8284@gmail.com>
2024-08-17 17:44:17 +0300

    feat@dissoc

--- files/index.d.ts
+++ files/index.d.ts
@@ -931,8 +931,8 @@ Notes:
 
 */
 // @SINGLE_MARKER
+export function dissoc<K extends PropertyKey>(prop: K): <U extends { [P in K]?: any}>(obj: string extends keyof U ? U : undefined extends U[K] ? U : never) => U;
 export function dissoc<U, K extends keyof U>(prop: string extends keyof U ? K : undefined extends U[K] ? K : never, obj: U): U;
-
 /*
 Method: divide
 
