615a8e2c60d95b91d7e70885a58a477abcfa9f93
Deyan Totev <deyan8284@gmail.com>
2024-08-18 09:07:41 +0300

    feat@fix modify

--- source/dissoc-spec.ts
+++ source/dissoc-spec.ts
@@ -1,31 +1,31 @@
-import {dissoc} from 'rambda'
+import { dissoc } from 'rambda';
 
 type Obj = {
-  str: string;
-  num: number;
-  opt?: boolean;
-  orUndefined: boolean | undefined;
-  orNull: boolean | null;
+	str: string;
+	num: number;
+	opt?: boolean;
+	orUndefined: boolean | undefined;
+	orNull: boolean | null;
 };
 
 const obj: Obj = { str: 'foo', num: 1, orUndefined: true, orNull: true };
 
 describe('R.dissoc', () => {
-  it('ramda tests', () => {
-    // @ts-expect-error
-    dissoc('str', obj)
-    // @ts-expect-error
-    dissoc('num', obj)
-    // @ts-expect-error
-    dissoc('orNull', obj)
-   
-    let result1 = dissoc('opt', obj)
-    result1 // $ExpectType Obj
-    // @ts-expect-error
-    dissoc('num')(obj)
-    let result2 = dissoc('orUndefined', obj)
-    result2 // $ExpectType Obj
-    let result3 = dissoc('opt')(obj)
-    result3 // $ExpectType Obj
-  })
-})
+	it('ramda tests', () => {
+		// @ts-expect-error
+		dissoc('str', obj);
+		// @ts-expect-error
+		dissoc('num', obj);
+		// @ts-expect-error
+		dissoc('orNull', obj);
+
+		const result1 = dissoc('opt', obj);
+		result1; // $ExpectType Obj
+		// @ts-expect-error
+		dissoc('num')(obj);
+		const result2 = dissoc('orUndefined', obj);
+		result2; // $ExpectType Obj
+		const result3 = dissoc('opt')(obj);
+		result3; // $ExpectType Obj
+	});
+});
