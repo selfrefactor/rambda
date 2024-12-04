3fad305759411c1e738a2e30324c8d5022db62e0
Dejan Toteff <deyan8284@gmail.com>
2024-12-04 09:53:46 +0200

    feat@fix r.diff.with

--- source/differenceWith.spec.js
+++ source/differenceWith.spec.js
@@ -1,13 +1,15 @@
-import { differenceWith } from './differenceWith.js'
+import { differenceWith } from './differenceWith.js';
 
-test('happy', () => {
-  const foo = [ { a : 1 }, { a : 2 }, { a : 3 } ]
-  const bar = [ { a : 3 }, { a : 4 } ]
-  const fn = function (r, s){
-    return r.a === s.a
-  }
-  const result = differenceWith(
-    fn, foo, bar
-  )
-  expect(result).toEqual([ { a : 1 }, { a : 2 } ])
-})
+const fn = (a, b) => a.x === b.x;
+
+test('same length of list', () => {
+	const result = differenceWith(fn, [{ x: 1 }, { x: 2 }], [{ x: 1 }, { x: 3 }]);
+	expect(result).toEqual([{ x: 2 }]);
+});
+
+test('different length of list', () => {
+	const foo = [{ x: 1 }, { x: 2 }, { x: 3 }];
+	const bar = [{ x: 3 }, { x: 4 }];
+	const result = differenceWith(fn, foo, bar);
+	expect(result).toEqual([{ x: 1 }, { x: 2 }]);
+});
