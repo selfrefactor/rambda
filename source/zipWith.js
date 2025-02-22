8819dea40ca3d1160a9fab05be7387dcfa710330
Dejan Toteff <deyan8284@gmail.com>
2025-02-22 23:13:57 +0200

    chore@small

--- source/zipWith.js
+++ source/zipWith.js
@@ -1,10 +1,8 @@
-import { curry } from './curry.js'
 import { take } from './take.js'
 
-function zipWithFn(fn, x, y) {
-  return take(x.length > y.length ? y.length : x.length, x).map((xInstance, i) =>
+export function zipWith(fn, x) {
+	return y => take(x.length > y.length ? y.length : x.length, x).map((xInstance, i) =>
     fn(xInstance, y[i]),
   )
 }
 
-export const zipWith = curry(zipWithFn)
