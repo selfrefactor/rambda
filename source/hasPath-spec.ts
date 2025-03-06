5dc5f79fc050f29e9ce0ccfcc6c201027808a2ca
Dejan Toteff <deyan8284@gmail.com>
2025-03-06 21:18:52 +0200

    chore@small

--- source/hasPath-spec.ts
+++ source/hasPath-spec.ts
@@ -1,16 +1,6 @@
 import { hasPath } from 'rambda'
 
 describe('R.hasPath', () => {
-  it('string path', () => {
-    const obj = { a: { b: 1 } }
-    const result = hasPath('a.b', obj)
-    const curriedResult = hasPath('a.c')(obj)
-    result // $ExpectType boolean
-    curriedResult // $ExpectType boolean
-  })
-  it('array path', () => {
-    const obj = { a: { b: 1 } }
-    const result = hasPath(['a', 'c'])(obj)
-    result // $ExpectType boolean
+  it('happy', () => {
   })
 })
--- source/path-spec.ts
+++ source/path-spec.ts
@@ -3,8 +3,7 @@ import { path, piped } from 'rambda'
 const input = { a: { b: { c: true } } }
 
 describe('R.path with string as path', () => {
-  it('without specified output type', () => {
-    
+  it('happy', () => {
 		let result = piped(
 			input,
 			path(['a','b',])
