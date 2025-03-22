da58c3ef5c111a1b62691f36775d5ae065d7ed1d
Dejan Toteff <deyan8284@gmail.com>
2025-03-22 08:02:58 +0200

    chore@small

--- source/partition.spec.js
+++ source/partition.spec.js
@@ -2,23 +2,8 @@ import { partition } from './partition.js'
 
 test('happy', () => {
   const list = [1, 2, 3]
-  const obj = {
-    a: 1,
-    b: 2,
-    c: 3,
-  }
   const predicate = x => x > 2
 
-  const result = [partition(predicate)(list), partition(predicate)(obj)]
-  const expected = [
-    [[3], [1, 2]],
-    [
-      { c: 3 },
-      {
-        a: 1,
-        b: 2,
-      },
-    ],
-  ]
-  expect(result).toEqual(expected)
+  const result = partition(predicate)(list)
+  expect(result).toEqual([[3], [1, 2]])
 })
--- source/partitionObject.spec.js
+++ source/partitionObject.spec.js
@@ -27,26 +27,3 @@ test('happy', () => {
 
   expect(result).toEqual(expectedResult)
 })
-
-test('readme example', () => {
-  const list = [1, 2, 3]
-  const obj = {
-    a: 1,
-    b: 2,
-    c: 3,
-  }
-  const predicate = x => x > 2
-
-  const result = [partition(predicate)(list), partition(predicate)(obj)]
-  const expected = [
-    [[3], [1, 2]],
-    [
-      { c: 3 },
-      {
-        a: 1,
-        b: 2,
-      },
-    ],
-  ]
-  expect(result).toEqual(expected)
-})
