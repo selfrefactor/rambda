8337fe9fff2ac159b332e69a4800e380fe97f4e2
Dejan Toteff <deyan8284@gmail.com>
2025-07-10 09:13:11 +0300

    chore@small

--- source/modifyPath.spec.js
+++ source/modifyPath.spec.js
@@ -4,3 +4,8 @@ test('happy', () => {
   const result = modifyPath('a.b.c', x => x + 1)({ a: { b: { c: 1 } } })
   expect(result).toEqual({ a: { b: { c: 2 } } })
 })
+
+test('works only on existing paths', () => {
+  const result = modifyPath('a.b.d', x => x + 1)({ a: { b: { c: 1 } } })
+  expect(result).toEqual({ a: { b: { c: 1 } } })
+})
