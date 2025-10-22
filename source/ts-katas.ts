2b5ff5cadcb10978d2e8f497d95e7a685219c268
Dejan Toteff <deyan8284@gmail.com>
2025-10-23 01:20:47 +0300

    2

--- source/ts-katas.ts
+++ source/ts-katas.ts
@@ -1,6 +1,41 @@
-import { IterableContainer, map, pipe,Mapped, MergeTypes } from 'rambda'
+import { IterableContainer, map, pipe,Mapped, MergeTypes, MergeTypesAlternative } from 'rambda'
 
 const list = [1, 2, 3]
+type ToFunctions<T> = T extends [infer E] ? (a: E) => void : never;
+type DeepPick<T, K> = K extends keyof T ? { [P in K]: T[P] } : K extends `${infer A extends keyof T & string}.${infer B}` ? { [P in A]: DeepPick<T[A], B> }
+: never
+type DeepPickAll<T, K extends string[]> = K extends [infer F, ...infer Rest extends string[]] ? MergeTypes<DeepPick<T, F> & DeepPickAll<T, Rest>> : {}
+
+type f = DeepPick<{ a: { b: { c: string } } }, 'a.b.c'>
+type f2 = DeepPickAll<{ b: 1,a: { b: { c: string } } }, ['a.b', 'b']>
+type f2s = MergeTypesAlternative<DeepPickAll<{ b: 1,a: { b: { c: string } } }, ['a.b.c', 'b']>>
+
+
+function deepPick<T, K extends string>(obj: T, key: K): DeepPick<T, K> {
+	return key.split('.').reduce((acc: any, k: string) => {
+		return acc?.[k]
+	}, obj) as DeepPick<T, K>
+}
+
+function deepPickAll<T, K extends string[]>(obj: T, keys: K): MergeTypes<DeepPickAll<T, K>> {
+	return keys.reduce((acc, key) => {
+		return { ...acc, ...deepPick(obj, key) }
+	}, {} as MergeTypes<DeepPickAll<T, K>>)
+}
+
+let l = deepPick({ a: { b: { c: 1 } } }, 'a.b.c')
+l.a.b.c
+let l2 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+let l3 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+let l4 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+let l5 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+let l6 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+let l7 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+let l8 = deepPickAll({ a: { b: { c: 1 } } }, ['a.b.c', 'a.b'])
+
+
+
+
 type SnakeCase<T> = T extends `${infer F}${infer R}`
 ? F extends Uppercase<F>
 ? `_${Lowercase<F>}${SnakeCase<R>}`
