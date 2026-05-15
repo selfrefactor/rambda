2629365bdaf80c443e3e6c502869fde7810d260f
Dejan Toteff <deyan8284@gmail.com>
2026-05-15 15:07:43 +0300

    1

--- source/equals-spec.ts
+++ source/equals-spec.ts
@@ -2,13 +2,13 @@ import { equals } from 'rambda'
 
 describe('R.equals', () => {
   it('happy', () => {
-    const result = equals(4, 1)
+    const result = equals(4)(1)
     result // $ExpectType boolean
   })
   it('with object', () => {
     const foo = { a: 1 }
     const bar = { a: 2 }
-    const result = equals(foo, bar)
+    const result = equals(foo)(bar)
     result // $ExpectType boolean
   })
   it('curried', () => {
